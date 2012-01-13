81fa58ae680e89db067806f73c742134d5e3342a
Dan <git@dandart.co.uk>
2012-01-13 02:01:37 +0000

    initial

---
+++ c/five.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    double in, out;
+    scanf("%lf", &in);
+    out = log(in);
+    printf("%lf\n", out);
+    return 0;
+}
---
+++ c/four.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int ch;
+    for (;; ) {
+        ch = getchar();
+        printf("%x",ch);
+    }
+}
+
