774759827af35ddf8d6bd89c5623f4f543668939
Dan <git@dandart.co.uk>
2015-04-05 17:47:26 +0100

    some maths

---
+++ js/maths/happy.js
@@ -0,0 +1,13 @@
+var n = Number(process.argv[2]);
+console.log(n);
+//for (var it = 1; it <= 10; it++) {
+while (1 != n && 145 != n) {
+	
+	var sn = n.toString(),
+		t = 0;
+	for (var i in sn) {
+		t += Math.pow(Number(sn[i]), 2);
+	}
+	n = Number(t);
+	console.log(n);
+}
