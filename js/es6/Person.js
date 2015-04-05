c3bfd41b50228d91a60f1580eff58859ec36de1f
Dan Dart <dandart@googlemail.com>
2015-04-05 17:47:36 +0100

    some es6

---
+++ js/es6/Person.js
@@ -0,0 +1,22 @@
+export default class Person
+{
+    constructor(name, age, genders)
+    {
+        this.name = name;
+        this.age = age;
+        this.genders = genders;
+    }
+
+    getName()
+    {
+        return this.name;
+    }
+}
+
+export class Me extends Person
+{
+    constructor(name, age)
+    {
+        super(name, age, ['all', 'of', 'them']);
+    }
+}
---
+++ js/es6/test.js
@@ -0,0 +1,4 @@
+import Person, Me from 'Person';
+
+let me = new Me('kath', 23);
+console.log(me.getName());
