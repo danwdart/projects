d4c30f7ddca260792a4a3d51cde52867ea506de5
Dan Dart <dandart@googlemail.com>
2015-12-20 15:44:37 +0000

    add a bunch of maths things like more grafting, fermat primes, flt, pytho, seq, trip including bugnumber.js

---
+++ js/maths/flt.js
@@ -0,0 +1,16 @@
+"use strict";
+import BN from 'bignumber.js';
+BN.config(10000,4);
+function flt(using, what)
+{
+	using = new BN(using);
+	what = new BN(what);
+	let inter = using.pow(what).minus(using);
+	return inter.dividedBy(what).floor( ).times(what).equals(inter);
+}
+for (var i = 0; i <= 1000; i++) {
+	if (flt(2, i))
+		console.log(i, 'is prime.');
+	else
+		console.log(i, 'is not prime.');
+}
---
+++ js/maths/seq.js
@@ -0,0 +1,19 @@
+"use strict";
+function bnot(n)
+{
+	return n.toString(2)
+		.replace('1','2')
+		.replace('0','1')
+		.replace('2','0');
+}
+
+function iter(n)
+{
+	return n << n.toString(2).length | bnot(n);
+}
+
+let begin = 1;
+for (let i = 0; i <= 20; i++) {
+	console.log(begin);
+	begin = iter(begin);
+}
