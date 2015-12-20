3e4a82798333401d963f5343712cc5b6167a7444
Dan Dart <dandart@googlemail.com>
2015-12-20 15:42:02 +0000

    test

---
+++ js/test.js
@@ -0,0 +1,25 @@
+var itt = function() {
+        var self = this;
+
+        self._elems = [];
+
+        self.add = function(e) {
+            self._elems.push(e);
+        };
+
+        self.forEach = function(cb) {
+            for (var i = 0; i < self._elems.length; i++) {
+                cb(self._elems[i], i);
+            }
+        } 
+    },
+    bob = new itt();
+
+bob.add('1');
+bob.add('2');
+bob.add('3');
+console.log('hi');
+bob.forEach(function(e) {
+    console.log(e);
+});
+console.log('ho');
