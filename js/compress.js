fa7c7c43644c140068f6794cc43930b3121fbdb7
Dan <git@dandart.co.uk>
2016-04-24 23:15:06 +0100

    add prime factor test

--- js/compress.js
+++ js/compress.js
@@ -14,6 +14,23 @@ let tryfunc = (name, num, squashFn, stretchFn) => {
     console.log(name+' method:', nums, 'with entropy', intFinalEntropy);
 };
 
+function prifact(num){
+  var root = Math.sqrt(num),
+  result = arguments[1] || [],  //get unnamed paremeter from recursive calls
+  x = 2;
+
+  if(num % x){//if not divisible by 2
+   x = 3;//assign first odd
+   while((num % x) && ((x = x + 2) < root)){}//iterate odds
+  }
+  //if no factor found then num is prime
+  x = (x <= root) ? x : num;
+  result.push(x);//push latest prime factor
+
+  //if num isn't prime factor make recursive call
+  return (x === num) ? result : prifact(num/x, result) ;
+}
+
 console.log('Generating big number');
 let intBigNumber = Math.floor(Math.random() * (1<<30));
 console.log('Number is', intBigNumber, 'entropy is', Math.ceil(Math.log2(intBigNumber)), 'bits');
@@ -26,3 +43,11 @@ console.log('Trying tesseract');
 tryfunc('Tesseract', intBigNumber, (n)=>n**(1/4), (n)=>n**4);
 console.log('Trying log');
 tryfunc('Log', intBigNumber, Math.log, Math.exp);
+console.log('Trying factoring');
+let facts = prifact(intBigNumber);
+console.log('Prime Factors', facts);
+let intTotalEnt = 0;
+for (let num of facts) {
+    intTotalEnt += Math.ceil(Math.log2(num));
+}
+console.log('Prime factors entropy is', intTotalEnt);
