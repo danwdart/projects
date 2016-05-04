7697be82f0da150babeb31f3fd350f4e8aca5fdd
Dan Dart <dandart@googlemail.com>
2016-05-05 00:31:45 +0100

    PowerMod tricks

---
+++ js/maths/powermod.js
@@ -0,0 +1,9 @@
+for (let base = 2; base <= 36; base++) {
+    for (let power = 2; power <= base; power++) {
+        let str = '';
+        for (let n = 1; n < base; n++) {
+            str += (Math.pow(n, power) % base).toString(base);
+        }
+        console.log('Power '+power+' mod '+base+': '+str);
+    }
+}
