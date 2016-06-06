d18c8e5d729978c00f937f5de2180efe2f9dbfa6
Dan Dart <dandart@googlemail.com>
2016-06-06 01:41:26 +0100

    call cancelling

--- js/webrtc/server.js
+++ js/webrtc/server.js
@@ -3,17 +3,11 @@ io.sockets.on('connection', function (socket) {
     socket.emit('message', { hello: 'from server' });
     socket.broadcast.emit('message', 'user connected');
     socket.on('message', function (data) {
-        console.log('message', data);
-        socket.broadcast.emit('message', {echo: data});
     });
     socket.on('desc', function(data) {
-        console.log('desc', data);
-        socket.broadcast.emit('desc', data);
         socket.broadcast.emit('message', {type: 'desc', data: data});
     });
     socket.on('candidate', function(data) {
-        console.log('candidate', data);
-        socket.broadcast.emit('candidate', data);
         socket.broadcast.emit('message', {type: 'candidate', data: data});
     });
     socket.on('disconnect', function() {
