f15de2d429a73a52967b89c1de1fbe99c0c04784
Dan <git@dandart.co.uk>
2016-07-02 04:44:39 +0100

    sweet mouse colours

--- js/maths/glprime/fragment.f.glsl
+++ js/maths/glprime/fragment.f.glsl
@@ -1,15 +1,17 @@
 precision highp float;
 
-uniform float h;
-uniform float w;
+uniform vec2 dim;
+uniform vec2 mouse;
 uniform float t;
 
 void main()
 {
-    float x = gl_FragCoord.x / w;
-    float y = gl_FragCoord.y / h;
+    float x = gl_FragCoord.x / dim.x;
+    float y = gl_FragCoord.y / dim.y;
+    float mxp = mouse.x / dim.x;
+    float myp = mouse.y / dim.y;
     float ms = mod(t,1000.0);
     float s = ms / 1000.0;
 
-    gl_FragColor = vec4(mod(x*y,s),mod(s,x),mod(y,s), 1.0);
+    gl_FragColor = vec4(mxp,myp,x*y, 1.0);
 }
