158544c8a961691671d8226ac738052679db31bb
Dan <git@dandart.co.uk>
2016-07-02 04:28:09 +0100

    ps

--- js/maths/primespiral/primespiral.js
+++ js/maths/primespiral/primespiral.js
@@ -9,37 +9,15 @@ canvas.style.height = h + 'px';
 let ctx = canvas.getContext('2d'),
     centre = [Math.floor(w/2), Math.floor(h/2)],
     currentpoint = centre,
-    isPrime = (n) => {
-         if (isNaN(n) || !isFinite(n) || n%1 || n<2) return false;
-         if (n==leastFactor(n)) return true;
-         return false;
-    },
-    leastFactor = (n) => {
-        if (isNaN(n) || !isFinite(n)) return NaN;
-        if (n==0) return 0;
-        if (n%1 || n*n<2) return 1;
-        if (n%2==0) return 2;
-        if (n%3==0) return 3;
-        if (n%5==0) return 5;
-        var m = Math.sqrt(n);
-            for (var i=7;i<=m;i+=30) {
-            if (n%i==0)      return i;
-            if (n%(i+4)==0)  return i+4;
-            if (n%(i+6)==0)  return i+6;
-            if (n%(i+10)==0) return i+10;
-            if (n%(i+12)==0) return i+12;
-            if (n%(i+16)==0) return i+16;
-            if (n%(i+22)==0) return i+22;
-            if (n%(i+24)==0) return i+24;
-        }
-        return n;
-    },
     plot = (point, colour) => {
         ctx.fillColor = colour;
         ctx.rect(...point,1,1);
         ctx.fill();
     },
     black = 'rgb(0,0,0)',
+    red = 'rgb(255,0,0)',
+    green = 'rgb(0,255,0)',
+    blue = 'rgb(0,0,255)',
     minx = 0,
     maxx = 0,
     miny = 0,
@@ -49,12 +27,16 @@ let ctx = canvas.getContext('2d'),
     no = 1,
     not = 0;
 
-for (let i = 0; i <= 100000; i++) {
+for (let i = 0; i <= 10000; i++) {
     //console.log(i);
 
-    if (isPrime(i)) {
-        plot(currentpoint, black)
-    }
+    let r = Math.pow(i,7) % 6,
+        map = [
+            black,
+            red,
+            green,
+            blue
+        ];
 
     if (2 == not) {
         //console.log('not reached 2, resetting, adding no');
@@ -84,4 +66,6 @@ for (let i = 0; i <= 100000; i++) {
         default: console.log('broken', d);
     }
     //console.log('currentpoint now', currentpoint)
+
+    plot(currentpoint, map[r]);
 }
