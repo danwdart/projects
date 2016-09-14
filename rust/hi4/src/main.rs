e0d4e3936ed3852f6efa1cb08855d5cfffd9155e
Dan <git@dandart.co.uk>
2016-09-14 01:47:21 +0100

    some more rust stuff

---
+++ rust/hi4/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let v = vec![1, 2, 3, 4, 5];
+    let w = vec![1; 3];
+    
+    let z: usize = 1;
+    
+    println!("{}, {}", v[z], w[z]);
+    
+    for i in &v {
+        println!("{}", i);
+    }
+}
