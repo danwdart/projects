12bbdbfa967738bb3bb23d3d4e99c7b8cca5f78d
Dan <git@dandart.co.uk>
2017-02-23 18:26:49 +0000

    php call private w/o reflection

---
+++ php/callPrivateWithoutReflection.php
@@ -0,0 +1,16 @@
+<?php
+class C
+{
+    private function _doSomething()
+    {
+        echo 'Oh no!'.PHP_EOL;
+    }
+}
+
+$o = new C();
+
+$cl = (function() {
+    return $this->_doSomething();
+})->bindTo($o, $o);
+
+$cl();
