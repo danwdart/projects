84257a3d0a19bc68589bcee8ae6d5440d9bf80ac
Dan <git@dandart.co.uk>
2017-02-24 20:46:44 +0000

    unprimer

---
+++ js/maths/unprime.js
@@ -0,0 +1,12 @@
+/*
+
+*/
+
+let c = Number(process.argv[2]);
+console.log(`Attempting to deconstruct ${c}`);
+
+let t = c;
+
+for (let i = 3; i <= t; i+=2) {
+    console.log('primesearch', t, i, t % i);
+}
