e11d4ac1768fe6c8f77a0b5ba5472410b51b5281
Dan Dart <git@dandart.co.uk>
2018-01-09 15:23:14 +0000

    slightly ugly class composing

---
+++ js/compose.js
@@ -0,0 +1,48 @@
+// The list way
+
+class B
+{
+    b()
+    {
+        return `b`;
+    }
+}
+
+class C
+{
+    c()
+    {
+        return `c`;
+    }
+}
+
+class D
+{
+    d()
+    {
+        return `d`;
+    }
+}
+
+const compose = arrClasses => {
+    const cl = class {};
+    arrClasses.forEach(c => {
+        Object.getOwnPropertyNames(c.prototype).filter(n => `constructor` !== n).forEach(m => {
+            cl.prototype[m] = c.prototype[m];
+        });
+    });
+    return cl;
+};
+
+class A extends compose([B, C, D]);
+{
+    constructor()
+    {
+        super();
+        console.log(this.b(), this.c(), this.d());
+    }
+}
+
+new A();
+
+// The decorated decorator way
