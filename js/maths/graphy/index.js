9ac4c5387c9e419cae02d1090a4148c1d96a6ea4
Dan <git@dandart.co.uk>
2018-01-21 18:28:14 +0000

    graphy

---
+++ js/maths/graphy/index.js
@@ -0,0 +1,16 @@
+import {cy, addNode, addEdge, refresh} from './lib.js';
+
+const numNodes = 30;
+
+for (let i = 1; i <= numNodes; i++) {
+    addNode(i);
+}
+for (let i = 1; i <= numNodes; i++) {
+    for (let j = 1; j <= numNodes; j++) {
+        let sq = Math.sqrt(i + j);
+        if (0 === sq - Math.floor(sq)) {
+            addEdge(i, j);
+        }
+    }
+}
+refresh();
