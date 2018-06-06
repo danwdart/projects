cee5e3462693c8d6bdc7482a81edf96a65ed1684
Dan Dart <git@dandart.co.uk>
2018-06-06 14:14:59 +0100

    myact

---
+++ js/myact/index.js
@@ -0,0 +1,3 @@
+const e = (t,o,c)=>`<${t}${o?` ${o.map(([k,v])=>`${k}="${v}`).join(` `)}`:``}>${c.join(``)}</${t}>`;
+
+console.log(e(`h1`,[[`class`,`magic`]],[e(`span`,[[`class`,`three`]],[`hi`])]));
