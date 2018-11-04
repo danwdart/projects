f3c01f2208003477a56e1037b428ce1ce138edb2
Dan Dart <git@dandart.co.uk>
2018-11-04 16:26:09 +0000

    Variants of foo, in a composed format

--- js/functional/monads/default-demo.js
+++ js/functional/monads/default-demo.js
@@ -8,13 +8,26 @@ const def = d => v => v || d;
 // I don't think that works tho
 const foo = f => g => x => y => f(g(x)(y));
 
+// Using these...
+const compose = f => g => x => f(g(x));
+const dup = f => x => f(x)(x);
+
+// foo can be shortened and made more confusing...
+
+const fooComposed1 = f => g => x => compose(f)(g(x));
+const fooComposed2 = f => compose(compose(f));
+const fooComposed3 = f => compose(compose)(compose)(f);
+const fooComposed4 = compose(compose)(compose);
+const fooComposed5 = dup(compose)(compose);
+const fooComposed6 = dup(dup)(compose);
+
 const defToObj = def({});
 
 const a = {};
 const b = prop("b")(a); // a.b
 const defB = defToObj(b); // a.b || {}
 const defBAlt = defToObj(prop("b")(a)); // a.b || {}
-const propDefObj = foo(defToObj)(prop); // y.x || {}
+const propDefObj = fooComposed6(defToObj)(prop); // y.x || {}
 
 const c = propDefObj("c")(defBAlt); // x = b; y = c; then use above
 
