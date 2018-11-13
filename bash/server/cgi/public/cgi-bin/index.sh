b840760ef5cabd9993271a0eb384cec369bcd71a
Dan Dart <git@dandart.co.uk>
2018-11-13 21:12:21 +0000

    envs in bash cgi

--- bash/server/cgi/public/cgi-bin/index.sh
+++ bash/server/cgi/public/cgi-bin/index.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
 echo "Content-Type: application/json"
 echo ""
-echo '{"your_information":true}'
+echo "{\
+    \"your_information\":true,\
+    \"query_string\":\"$QUERY_STRING\",\
+    \"user-agent\":\"$HTTP_USER_AGENT\",\
+    \"cookie\":\"$HTTP_COOKIE\",\
+    \"referrer\":\"$HTTP_REFERER\",\
+    \"remote addr\":\"$REMOTE_ADDR\",\
+    \"remote host\":\"$REMOTE_HOST\",\
+    \"request method\":\"$REQUEST_METHOD\",\
+    \"script name\":\"$SCRIPT_NAME\",\
+    \"server name\":\"$SERVER_NAME\",\
+    \"server port\":\"$SERVER_PORT\",\
+    \"server protocol\":\"$SERVER_PROTOCOL\",\
+    \"server software\":\"$SERVER_SOFTWARE\",\
+    \"uid\":\"$UID\",\
+    \"user\":\"$USER\"\
+    }"
