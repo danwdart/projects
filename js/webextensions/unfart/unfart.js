9627eff5ad04e812b6c0b618252f4f310bd37f4e
Dan Dart <git@dandart.co.uk>
2018-12-21 19:59:49 +0000

    farthole b mcgee

--- js/webextensions/unfart/unfart.js
+++ js/webextensions/unfart/unfart.js
@@ -1,19 +1,30 @@
 console.log(`Unfart Initialised`);
-window.addEventListener('load', () => 
-    new Map([
-        [
-            /Donald John Trump/i,
-            `Farthole McGee`
-        ],
-        [
-            /Donald Trump/i,
-            `Did a Fart`
-        ],
-        [
-            /Trump/,
-            `Fart`
-        ]
-    ]).forEach((v, k) => document.documentElement.innerHTML = document.documentElement.innerHTML.replace(new RegExp(k, 'g'), v))
-);
+const doReplaceName = new Map([
+    [
+        /Donald John Trump/i,
+        `Farthole Butthole McGee`
+    ],
+    [
+        /Donald J. Trump/i,
+        `Farthole B. McGee`
+    ],
+    [
+        /Donald Trump/i,
+        `Did a Fart`
+    ],
+    [
+        /Trump/,
+        `Fart`
+    ]
+]).forEach((v, k) => document.documentElement.innerHTML = document.documentElement.innerHTML.replace(new RegExp(k, 'g'), v));
+
+const doReplaceTweets = "https://twitter.com/realdonaldtrump" === window.location.href ?
+    [].forEach.call(document.querySelectorAll('.TweetTextSize'), p => p.innerHTML = "Poo poo poo.") :
+    () => {};
+
+const doReplace = () => doReplaceName() && doReplaceTweets();
+
+window.addEventListener('load', doReplace);
+doReplace();
 
 console.log(`Unfart Finished`);
