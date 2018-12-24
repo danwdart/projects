86c6ebedb485e423c9535ce8387a106724ac8839
Dan Dart <git@dandart.co.uk>
2018-12-24 22:36:50 +0000

    functional reusability

--- js/rolling/index.js
+++ js/rolling/index.js
@@ -1,44 +1,94 @@
 const video = document.querySelector(`video`),
     canvas = document.querySelector(`canvas`),
     ctx = canvas.getContext(`2d`),
+    WIDTH = 640,
     HEIGHT = 480,
-    PIXELS_PER_FRAME = 1,
+    PIXELS_PER_FRAME = 2,
+    drawTo = ctx => sourceImage => ({
+        src: {
+            x: sourceX,
+            y: sourceY,
+            width: sourceWidth,
+            height: sourceHeight
+        },
+        dest: {
+            x: destX,
+            y: destY,
+            width: destWidth,
+            height: destHeight
+        }
+    }) => ctx.drawImage(
+        sourceImage,
+        sourceX,
+        sourceY,
+        sourceWidth,
+        sourceHeight,
+        destX,
+        destY,
+        destWidth,
+        destHeight
+    ),
+    copyFrom = ctx => sourceImage => params => drawTo(ctx)(sourceImage)({
+        src: params,
+        dest: params
+    }),
+    copy = copyFrom(ctx)(video),
     animate = () => {
-        const sourceImage = video,
-            sourceX = 0,
-            sourceY = row,
-            sourceWidth = 640,
-            sourceHeight = 1,
-            destX = 0,
-            destY = row,
-            destWidth = 640,
-            destHeight = 1;
+        copy({
+            x: 0,
+            y: row,
+            width: WIDTH,
+            height: PIXELS_PER_FRAME
+        });
 
-        ctx.drawImage(
-            sourceImage,
-            sourceX,
-            sourceY,
-            sourceWidth,
-            sourceHeight,
-            destX,
-            destY,
-            destWidth,
-            destHeight
-        );
+        copy({
+            x: 0,
+            y: HEIGHT - row,
+            width: WIDTH,
+            height: PIXELS_PER_FRAME
+        });
+
+        copy({
+            x: col,
+            y: 0,
+            width: PIXELS_PER_FRAME,
+            height: HEIGHT
+        });
+
+        copy({
+            x: WIDTH - col,
+            y: 0,
+            width: PIXELS_PER_FRAME,
+            height: HEIGHT
+        });
+
         row += PIXELS_PER_FRAME;
         row %= HEIGHT;
+
+        col += PIXELS_PER_FRAME;
+        col %= WIDTH;
         requestAnimationFrame(animate);
     };
 
-let row = 0;
+let row = 0,
+    col = 0;
 
-navigator.mediaDevices.getUserMedia({
-    video: true,
-    audio: false
-}).then((mediaStream) => {
-    video.srcObject = mediaStream;
-    video.addEventListener(`loadedmetadata`, (ev) => {
-        video.play();
-        requestAnimationFrame(animate);
-    });
-}).catch((err) => console.log(err));
+(async () => {
+    try {
+        const mediaStream = await navigator.mediaDevices.getUserMedia({
+            video: true,
+            audio: false
+        });
+        
+        video.srcObject = mediaStream;
+
+        video.addEventListener(`loadedmetadata`, (ev) => {
+            video.play();
+            requestAnimationFrame(animate);
+        });
+    } catch (err) {
+        console.error(err);
+    }
+})();
+
+
