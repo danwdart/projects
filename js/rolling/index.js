a07243646533427ba040c91a624f55f6a872493e
Dan Dart <git@dandart.co.uk>
2018-12-24 20:33:04 +0000

    Explain rolling shutter code

--- js/rolling/index.js
+++ js/rolling/index.js
@@ -1,10 +1,32 @@
 const video = document.querySelector(`video`),
     canvas = document.querySelector(`canvas`),
     ctx = canvas.getContext(`2d`),
+    HEIGHT = 480,
+    PIXELS_PER_FRAME = 1,
     animate = () => {
-        ctx.drawImage(video, 0, row, 640, 10, 0, row, 640, 10);
-        row++;
-        row %= 480;
+        const sourceImage = video,
+            sourceX = 0,
+            sourceY = row,
+            sourceWidth = 640,
+            sourceHeight = 1,
+            destX = 0,
+            destY = row,
+            destWidth = 640,
+            destHeight = 1;
+
+        ctx.drawImage(
+            sourceImage,
+            sourceX,
+            sourceY,
+            sourceWidth,
+            sourceHeight,
+            destX,
+            destY,
+            destWidth,
+            destHeight
+        );
+        row += PIXELS_PER_FRAME;
+        row %= HEIGHT;
         requestAnimationFrame(animate);
     };
 
