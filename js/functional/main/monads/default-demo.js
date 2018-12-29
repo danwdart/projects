56a620ca6db6630b31c4f12a5047c26655845bb4
Dan Dart <git@dandart.co.uk>
2018-12-29 18:26:49 +0000

    foo was blackbird all along

--- js/functional/main/monads/default-demo.js
+++ js/functional/main/monads/default-demo.js
@@ -2,24 +2,22 @@ import {compose, dup} from './lib/functional';
 import {prop} from './lib/object';
 import {def} from './lib/value';
 
-// a.b.c.d.e.f.g
+// I use this combinator a lot, turns out it was called blackbird!
+//const blackbird = f => g => x => y => f(g(x)(y));
 
-// I use this combinator a lot, I wonder what it's called?
-//const foo = f => g => x => y => f(g(x)(y));
-
-// foo can be shortened and made more confusing...
-//const fooComposed1 = f => g => x => compose(f)(g(x));
-//const fooComposed2 = f => compose(compose(f));
-//const fooComposed3 = f => compose(compose)(compose)(f);
+// blackbird can be shortened and made more confusing...
+//const blackbirdComposed1 = f => g => x => compose(f)(g(x));
+//const blackbirdComposed2 = f => compose(compose(f));
+//const blackbirdComposed3 = f => compose(compose)(compose)(f);
 // we're finally pointless :p
-//const fooComposed4 = compose(compose)(compose);
-//const fooComposed5 = dup(compose)(compose);
-const fooComposed6 = dup(dup)(compose);
+//const blackbirdComposed4 = compose(compose)(compose);
+//const blackbirdComposed5 = dup(compose)(compose);
+const blackbirdComposed6 = dup(dup)(compose);
 // still at 3 fns, not going to get better than that.
 
 // if we reverse and get pointy this gets way more confusing...
-// const fooComposed7 = (f => x => f(x)(x))(f => x => f(x)(x))(f => g => x => f(g)(x))
-// console.log(fooComposed6(s => s.toUpperCase())(x => y => x + y)("hello ")("world"))
+// const blackbirdComposed7 = (f => x => f(x)(x))(f => x => f(x)(x))(f => g => x => f(g)(x))
+// console.log(blackbirdComposed6(s => s.toUpperCase())(x => y => x + y)("hello ")("world"))
 
 const defToObj = def({});
 
@@ -27,7 +25,7 @@ const a = {};
 //const b = prop('b')(a); // a.b
 //const defB = defToObj(b); // a.b || {}
 //const defBAlt = defToObj(prop('b')(a)); // a.b || {}
-const propDefObj = fooComposed6(defToObj)(prop); // y.x || {}
+const propDefObj = blackbirdComposed6(defToObj)(prop); // y.x || {}
 
 //const c = propDefObj('c')(defBAlt); // x = b; y = c; then use above
 
