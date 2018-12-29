04f06956f2226e6761c6e1513d9524a12628e963
Dan Dart <git@dandart.co.uk>
2018-12-29 18:27:11 +0000

    clean gamenu js

--- js/gamenu/js/index.js
+++ js/gamenu/js/index.js
@@ -6,35 +6,9 @@ const eq = a => b => a === b;
 
 const compose = f => g => x => f(g(x));
 const flip = f => y => x => f(x)(y);
-//const call = f => x => f(x);
-//const applyTo = x => f => f(x);
-
 const find = on => fn => on.find(fn);
-
 const on = f => g => x => y => f(g(x))(g(y));
-
-//const b3 = f => g => h => x => f(g(h(x)));
 const d2 = f => g => h => i => j => f(g(h))(i(j));
-/*
-"D" = B2
-"B1" = B3
-"D1" = B4
-"B3" = B5
-"D2" = B6
-"E" = B7
-"B2" = B8
-"Ê" =  E(E)
-
-B(B) = B^2 = B2
-B(B(B)) = B(B2) = B^3 = B4
-B(B(B(B))) = B^4 = B(B^3) = B(B4) = B8
-
-???
-B(B(B(B(B)))) = B^5 = B(B^4) = B(B8) = B16
-???
-
-B^x = B((x-1)^2)
-*/
 
 const findMenuItem = compose(find(json))(flip(d2(eq)(on(compose)(prop)('menu')('dataset')))(prop('id')));
 
@@ -45,8 +19,6 @@ const Maybe = x => ({
     map: f => x ? Maybe(f(x)) : Maybe(null),
     //ap: v => Maybe(x(v.unwrap()))
 });
-
-//const ifThen = then => iff => iff ? then : null;
 
 const getData = compose(flip(compose)(prop('dataset')))(prop);
 
