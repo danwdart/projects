e8f939545d400f49cdd4a8c2541003fd438089d8
Dan Dart <git@dandart.co.uk>
2019-03-01 04:05:14 +0000

    maybe work

--- haskell/moonmadness.hs
+++ haskell/moonmadness.hs
@@ -1,4 +1,12 @@
+import Control.Monad
 import Data.Function
+import Data.List
+
+splitEvery :: Int -> [a] -> [[a]]
+splitEvery _ [] = []
+splitEvery n list = first : (splitEvery n rest)
+  where
+    (first,rest) = splitAt n list
 
 splitIntIntoDigits :: Int -> [Int]
 splitIntIntoDigits i = map (\x -> (read [x])) $ show i
@@ -6,17 +14,32 @@ splitIntIntoDigits i = map (\x -> (read [x])) $ show i
 reduceDigitsIntoInt :: [Int] -> Int
 reduceDigitsIntoInt d = read $ concat $ map show d :: Int
 
-moonArithCol :: (Int -> Int -> Int) -> [Int] -> [Int] -> [Int]
-moonArithCol fn a b = reverse $ take (maxLength a b) $ zipWith fn (rppZeroes a) (rppZeroes b) where
+maxLength :: [a] -> [a] -> Int
+maxLength = on max length
+
+minLength :: [a] -> [a] -> Int
+minLength = on min length
+
+moonAddWith :: (Int -> Int -> Int) -> [Int] -> [Int] -> [Int]
+moonAddWith fn a b = reverse $ take (maxLength a b) $ zipWith fn (rppZeroes a) (rppZeroes b) where
     zeroes = repeat 0
-    maxLength = on max length
     rppZeroes n = reverse n ++ zeroes
 
+moonMulWith :: (Int -> Int -> Int) -> (Int -> Int -> Int) -> [Int] -> [Int] -> [Int]
+moonMulWith fnMul fnAdd a b = foldl1 (moonAddWith fnAdd) (mulTwo fnMul a b)
+
+mulTwo fnMul a b = transpose (concat ((transpose $ splitEvery maxLen (liftM2 fnMul a b)):([threeTwoOneZero minLen]))) where
+    maxLen = maxLength a b
+    minLen = minLength a b
+    threeTwoOneZero n = reverse $ tail $ take n $ inits $ repeat 0
+
 (<<+>>) :: Int -> Int -> Int
-a <<+>> b = reduceDigitsIntoInt $ moonArithCol max (splitIntIntoDigits a) (splitIntIntoDigits b)
+a <<+>> b = reduceDigitsIntoInt $ moonAddWith (max (splitIntIntoDigits a) (splitIntIntoDigits b))
 
 (<<*>>) :: Int -> Int -> Int
-(<<*>>) = undefined
-
+a <<*>> b = reduceDigitsIntoInt $ moonMulWith (min max (splitIntIntoDigits a) (splitIntIntoDigits b)
+)
 main :: IO ()
-main = putStrLn $ "35 <<+>> 97 is " ++ show (39 <<+>> 97 :: Int)
+main = mapM_ putStrLn [
+    "35 <<+>> 97 is " ++ show (39 <<+>> 97 :: Int),
+    "315 <<*>> 971 is " ++ show (315 <<*>> 971 :: Int)]
