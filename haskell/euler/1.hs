e8ff15c1897279b83b7a355e14a20ed2abdde6ad
Dan Dart <git@dandart.co.uk>
2019-03-09 16:35:47 +0000

    haskells

---
+++ haskell/euler/1.hs
@@ -0,0 +1,8 @@
+result1 :: Int
+result1 = sum $ filter (\x -> mod x 5 == 0 || mod x 3 == 0) [0..999]
+
+result2 :: Int
+result2 = sum $ filter (\x -> any (\y -> mod x y == 0) [3,5]) [0..999]
+
+main :: IO ()
+main = print result2
---
+++ haskell/euler/3.hs
@@ -0,0 +1,4 @@
+-- soe
+primes = 
+
+primefactors = 
