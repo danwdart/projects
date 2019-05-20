6dbcd8b942057004c499b41a9c662c70e2b7e2f0
Dan Dart <git@dandart.co.uk>
2019-05-20 01:42:54 +0100

    more primeyness

--- haskell/maths/primefun/primefun.hs
+++ haskell/maths/primefun/primefun.hs
@@ -1,6 +1,11 @@
 import Data.Numbers.Primes
 
+sumfact :: [(Integer, Integer)]
 sumfact = [(n, sum $ primeFactors n) | n <- [1..1000]]
+
+countfact :: [(Integer, Integer)]
+countfact = [(n, length $ primeFactors n) | n <- [1..1000]]
+
 
 main :: IO ()
 main = undefined
