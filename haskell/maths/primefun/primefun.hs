4b56802b8f8e79f5eacecb219bda45e22585acf1
Dan Dart <git@dandart.co.uk>
2019-05-20 01:47:50 +0100

    imports for on and nubBy

--- haskell/maths/primefun/primefun.hs
+++ haskell/maths/primefun/primefun.hs
@@ -1,11 +1,14 @@
+import Data.Function
+import Data.List
 import Data.Numbers.Primes
 
 sumfact :: [(Integer, Integer)]
 sumfact = [(n, sum $ primeFactors n) | n <- [1..1000]]
 
-countfact :: [(Integer, Integer)]
+countfact :: [(Integer, Int)]
 countfact = [(n, length $ primeFactors n) | n <- [1..1000]]
 
+-- nubBy (on (==) snd) countfact
 
 main :: IO ()
 main = undefined
