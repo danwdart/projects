8a32f92ff508d71d0c1cb10c781c14d83e9b885c
Dan Dart <git@dandart.co.uk>
2019-05-20 02:29:21 +0100

    eventLoop

--- haskell/eventLoop.hs
+++ haskell/eventLoop.hs
@@ -1,1 +1,11 @@
 -- Let's make our own event loop
+
+data EventType = FileEvent | MouseEvent | KeyEvent
+
+data Event = Event {
+    eventType :: EventType,
+    time :: Int,
+    dataString :: String
+}
+
+
