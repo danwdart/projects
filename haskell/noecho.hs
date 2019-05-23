caa4de36abdca53da0007e3205b18e16c1a08b1b
Dan Dart <git@dandart.co.uk>
2019-05-23 16:47:19 +0100

    hSetEcho

---
+++ haskell/noecho.hs
@@ -0,0 +1,9 @@
+import System.IO
+
+main :: IO ()
+main = do
+    hSetEcho stdin False
+    putStr "Enter your super secret password: "
+    y <- getLine
+    putStrLn $ "\nSo... it sounds like your password might have been " ++ y
+    putStrLn "Sorry about that."
