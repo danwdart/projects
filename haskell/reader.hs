84eb347d052380a6481f414b2e1dcd60713aab7d
Dan Dart <git@dandart.co.uk>
2019-06-02 03:01:20 +0100

    more reader

--- haskell/reader.hs
+++ haskell/reader.hs
@@ -1,10 +1,33 @@
 import Control.Monad.Reader
 
-myReader :: (Num a, MonadReader a m) => m a
-myReader = reader (+1)
+-- Reader a b is a -> b
 
-run :: Int
-run = runReader myReader 11
+intToIntReader :: Reader Int Int
+intToIntReader = reader (+1)
+
+stringToStringReader :: Reader String String
+stringToStringReader = reader (++"Bob")
+
+stringToIntReaderComplex :: Reader String Int
+stringToIntReaderComplex = local (++"Bobby") $ asks length
+
+rtSimple :: ReaderT String IO ()
+rtSimple = ask >>= liftIO . putStrLn
+
+rt :: ReaderT String IO ()
+rt = ReaderT $ do
+    x <- ask
+    y <- asks (++"Bob")
+    liftIO . putStrLn
+
+rWithLocal :: Reader String String
+rWithLocal = local (++"Jim") stringToStringReader
 
 main :: IO ()
-main = print run
+main = do
+    print $ runReader intToIntReader 11
+    print $ runReader stringToStringReader "Bob"
+    print $ runReader rWithLocal "Ted"
+    print $ runReader stringToIntReaderComplex "Bobby"
+    runReaderT rtSimple "Bob"
+    runReaderT rt "Bob"
