fbfe843fddec105f7348f7c48df41096aa694acd
Dan Dart <git@dandart.co.uk>
2019-06-02 17:01:16 +0100

    ioref

--- haskell/src/ioref.hs
+++ haskell/src/ioref.hs
@@ -1,1 +1,13 @@
-main = undefined
+import Data.IORef
+
+main1 = newIORef 0 >>= (\r -> modifyIORef r (+2) >> readIORef r >>= print)
+
+main2 = do
+    r <- newIORef "Hi "
+    modifyIORef r (++"Bob")
+    x <- readIORef r
+    putStrLn x
+
+main = do
+    main1
+    main2
