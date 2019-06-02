0b1ec6cd833aebd09fe9e5ec79d85bf745750b4c
Dan Dart <git@dandart.co.uk>
2019-06-02 17:01:31 +0100

    stm

--- haskell/src/stm.hs
+++ haskell/src/stm.hs
@@ -1,1 +1,15 @@
-main = undefined
+import Control.Monad.STM
+import Control.Concurrent.STM.TVar
+
+main1 = atomically (newTVar 1 >>= (\t -> writeTVar t 1 >> readTVar t)) >>= print
+
+tv2 = do
+    x <- newTVar 1
+    writeTVar x 1
+    readTVar x
+
+main2 = atomically tv2 >>= print
+
+main = do
+    main1
+    main2
