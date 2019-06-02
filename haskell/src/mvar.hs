34d477164b77dfb1cb9a955dbeb4c2fd0d0fc93d
Dan Dart <git@dandart.co.uk>
2019-06-02 17:01:22 +0100

    mvar

--- haskell/src/mvar.hs
+++ haskell/src/mvar.hs
@@ -1,1 +1,14 @@
-main = undefined
+import Control.Concurrent
+import Control.Concurrent.Async
+import Control.Concurrent.MVar
+
+main1 = newEmptyMVar >>= (\x -> putMVar x 1 >> takeMVar x) >>= print
+main2 = do
+    x <- newEmptyMVar
+    putMVar x 1
+    y <- takeMVar x
+    print y
+
+main = do
+    main1
+    main2
