19bd92eab4ae3eedcbdee42abad7365f9c195470
Dan Dart <git@dandart.co.uk>
2019-06-02 03:01:33 +0100

    lenses

---
+++ haskell/lenses.hs
@@ -0,0 +1,39 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+import Control.Monad
+import Control.Lens
+
+a = (1, 2, 3)
+
+data MyStruct = MyStruct {
+    _name :: String,
+    _age :: Int,
+    _friends :: [MyStruct]
+} deriving (Show)
+
+makeLenses ''MyStruct
+
+bob :: MyStruct
+bob = MyStruct {
+    _name = "Bob",
+    _age = 27,
+    _friends = [
+        MyStruct {
+            _name = "Jim",
+            _age = 27,
+            _friends = []
+        }
+    ]
+}
+
+main :: IO ()
+main = do
+    print a
+    print $ a^._2
+    print $ set _2 42 a
+    print $ _2 .~ 42 $ a
+    print $ a^._2
+    print $ "Bob"^.to length
+    print $ view _2 (1, 2)
+    print $ ((1,0),(2,"Two"),(3,0))^._2._2.to length
+    print $ bob^.name ++ show (bob^.age) ++ bob^.friends.ix 0.name
