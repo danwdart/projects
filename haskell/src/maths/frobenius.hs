e997168e4d3e562f1e38ff0cbe8be1cce77c9af5
Dan Dart <git@dandart.co.uk>
2019-06-29 21:57:38 +0100

    frobeniuses

---
+++ haskell/src/maths/frobenius.hs
@@ -0,0 +1,29 @@
+import Data.Either
+import System.Environment
+import Text.Read
+
+fromEither :: Either a a -> a
+fromEither (Left x) = x
+fromEither (Right y) = y
+
+frob2 :: Int -> Int -> Either String Int
+frob2 a b = if 1 == gcd a b then Right (frob2Real a b) else Left "Numbers cannot be frobeniused!"
+
+frob2Real :: Int -> Int -> Int
+frob2Real a b = a * b - a - b
+
+parseArgs :: [String] -> [Either String Int]
+parseArgs = map readEither
+
+areArgsValid :: [Either String Int] -> Bool
+areArgsValid args = all isRight args && 2 == length args
+
+validateArgs :: [Either String Int] -> Either String [Int]
+validateArgs xs = if areArgsValid xs then sequence xs else Left "Arguments not valid"
+
+processArgs :: Either String [Int] -> Either String Int
+processArgs (Right [a, b]) = frob2 a b
+processArgs _ = Left "Can't process args!"
+
+main :: IO ()
+main = getArgs >>= return . parseArgs >>= return . validateArgs >>= return . processArgs >>= return . fmap show >>= return . fromEither >>= putStrLn
