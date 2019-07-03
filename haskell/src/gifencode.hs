68fc9ad10f85a0f85510dce364772a4884635b22
Dan Dart <git@dandart.co.uk>
2019-07-03 19:37:01 +0100

    gifencode blank

---
+++ haskell/src/gifencode.hs
@@ -0,0 +1,6 @@
+import Graphics.GD
+--  Let's make things into gifs, ideally lossless videos, encoded, for the lels
+-- This needs the so file though, so I guess I should make a new docker image...
+
+main :: IO ()
+main = undefined
