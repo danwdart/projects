bf891cc975601b8789ab5c985fdb436ec9404533
Dan Dart <git@dandart.co.uk>
2019-07-04 22:51:21 +0100

    Some kind of combinator with gifencode that does not quite work

--- haskell/src/gifencode.hs
+++ haskell/src/gifencode.hs
@@ -7,6 +7,17 @@ main = do
     i <- newImage (256,256)
     drawFilledRectangle (0,0) (255,255) (rgb 255 255 255) i
     drawLine (100,100) (200,200) (rgb 0 0 0) i
-    drawLine (100,100) (200,200) (rgb 0 0 0) i
     setPixel (10,10) (rgb 0 0 0) i
     saveGifFile "bob.gif" i
+
+    -- so what about a combinator for \f x -> f x >> return x for things that return IO () but I want the result from?
+    -- idk maybe better in the state monad or the env monad?
+
+(>>>=) :: Monad m => m b -> (b -> m a) -> m b
+x >>>= f = x >>= f >> x
+infixl 1 >>>=
+
+main2 = newImage (256, 256) >>>=
+        drawFilledRectangle (0, 0) (255, 255) (rgb 255 255 255) >>>=
+        drawLine (100, 100) (200, 200) (rgb 0 0 0) >>>=
+        saveGifFile "jim.gif"
