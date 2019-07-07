4744c6534b32ffaaede3b5abdfeebcb339972f34
Dan Dart <git@dandart.co.uk>
2019-07-07 16:36:24 +0100

    DB stuff

--- haskell/src/db.hs
+++ haskell/src/db.hs
@@ -1,2 +1,21 @@
+{-# LANGUAGE OverloadedStrings #-} -- Text can accept strings - no need to pack
+{-# LANGUAGE ExtendedDefaultRules #-} -- Stringy stuff can default to text
+
+import Control.Monad.Trans (liftIO)
+
+import Database.HDBC
+import Database.HDBC.MySQL
+
+import Database.MongoDB
+
+import Data.Text (pack)
+
 -- import Database.MongoDB
-main = undefined
+main = do
+    mysqlconn <- connectMySQL $ MySQLConnectInfo "172.18.0.3" "sm" "sm" "sm" 3306 "" Nothing
+    ts <- quickQuery mysqlconn "show tables;" []
+
+    mongoconn <- connect $ host "localhost"
+    access mongoconn master "bob" ((insert "bobs" ["name"=:"Jim"]) >> find (select [] "bobs") >>= rest)
+
+    return ()
