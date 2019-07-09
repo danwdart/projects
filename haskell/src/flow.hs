e910f5c8b818a641724c5b4d98ed8c28a0640037
Dan Dart <git@dandart.co.uk>
2019-07-09 18:39:48 +0100

    Flow library

--- haskell/src/flow.hs
+++ haskell/src/flow.hs
@@ -1,3 +1,9 @@
 import Flow
 
-main = undefined
+main = do
+    1 |> (+2) |> (+3) |> print
+    print <| (+3) <| (+2) <| 1
+    print <| (+2) .> (+3) <| 3
+    print <| (+2) <. (+3) <| 3
+    3 |> (+2) <. (+1) |> print
+    3 |> (+2) .> (+1) |> print
