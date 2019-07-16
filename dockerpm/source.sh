e45e73089c5992062799a2ad349b30dd99420210
Dan Dart <git@dandart.co.uk>
2019-07-16 07:44:18 +0100

    add user mode docker

--- dockerpm/source.sh
+++ dockerpm/source.sh
@@ -1,20 +1,71 @@
+# Most of this stuff is rootless
 d() {
+    docker $@
+}
+
+sd() {
     sudo docker $@
+}
+
+doco() {
+    if [ -f docker-compose.yml ]
+    then
+        docker-compose -f docker-compose.yml $@
+    else
+        if [ -f ~/docker-compose.user.yml ]
+        then
+            docker-compose -f ~/docker-compose.user.yml $@
+        else
+            echo "No docker-compose.user.yml found in current path or home directory."
+        fi
+    fi
+}
+
+sdoco() {
+    if [ -f docker-compose.yml ]
+    then
+        sudo docker-compose -f docker-compose.yml $@
+    else
+        if [ -f ~/docker-compose.system.yml ]
+        then
+            sudo docker-compose -f ~/docker-compose.system.yml $@
+        else
+            echo "No docker-compose.system.yml found in current path or home directory."
+        fi
+    fi
 }
 
 dc() {
     if [ -f docker-compose.dev.yml ]
     then
-        sudo docker-compose -f docker-compose.yml -f docker-compose.dev.yml $@
+        doco -f docker-compose.dev.yml $@
     else
-        sudo docker-compose $@
+        doco $@
+    fi
+}
+
+sdc() {
+    if [ -f docker-compose.dev.yml ]
+    then
+        sdoco -f docker-compose.dev.yml $@
+    else
+        sdoco $@
     fi
 }
 
 dca() {
     if [ -f docker-compose.admin.yml ]
     then
-        dc -f docker-compose.admin.yml $@ 
+        sdc -f docker-compose.admin.yml $@ 
+    else
+        echo "No docker-compose.admin.yml present."
+    fi
+}
+
+sdca() {
+    if [ -f docker-compose.admin.yml ]
+    then
+        sdc -f docker-compose.admin.yml $@ 
     else
         echo "No docker-compose.admin.yml present."
     fi
@@ -55,20 +106,68 @@ alias dlf='d logs -f'
 alias ds='d search'
 alias dsp='d system prune -af'
 
-alias dhrr='drr -v $PWD:$PWD -w $PWD --net host -it'
-alias dhr='dhrr -u $UID:$GROUPS -v /etc/passwd:/etc/passwd -v /etc/group:/etc/group -e HOME=$HOME'
-alias dhrx='dhr -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix'
-alias dhrrx='dhrr -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix'
-alias dhrj='dhrx '
+alias sdcu='sdc up -d'
+alias sdcd='sdc down'
+alias sdce='sdc exec'
+alias sdcr='sdc run'
+alias sdcrr='sdc run --service-ports --rm'
 
-alias dhr-node='dhr -v $HOME:$HOME -v $PWD:$PWD node:alpine'
+alias sdcl='sdc logs'
+alias sdclf='sdc logs -f'
+alias sdcp='sdc ps'
+
+alias sdcsta='sdc start'
+alias sdcsto='sdc stop'
+
+alias sdcre='sdc restart'
+
+alias sdr='sd run'
+alias sdri='sd run -it'
+alias sdrd='sd run -d'
+
+alias sdsta='sd start'
+alias sdsto='sd stop'
+
+alias sdre='sd restart'
+
+alias sdrr='sd run --rm'
+alias sdrri='sdrr -it'
+alias sdrrd='sdrr -d'
+
+alias sde='sd exec'
+alias sdp='sd ps'
+alias sdl='sd logs'
+alias sdlf='sd logs -f'
+alias sds='sd search'
+alias sdsp='sd system prune -af'
+
+# Run in a home env (fake root)
+alias dhr='drr -v $PWD:$PWD -w $PWD --net host -it'
+
+# Run in a home env (fake root, X)
+alias dhrx='dhr -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix'
+
+
+# Run in a home env (real root)
+alias sdhr='sdrr -v $PWD:$PWD -w $PWD --net host -it'
+
+# Run in a home env (real root or defined user, X)
+alias sdhrrx='sdhr -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix'
+
+
+# Run in a home env (using root-owned daemon, run as user, X)
+alias sdhrx='sdhrrx -v /etc/passwd:/etc/passwd -v /etc/group:/etc/group -u $UID:$GROUPS'
+
+# Apps
+
+alias dhr-node='dhr -v $HOME:/root -v $PWD:$PWD node:alpine'
 
 alias node='dhr-node node'
 alias npm='dhr-node npm'
 alias npx='dhr-node npx'
 alias yarn='dhr-node yarn'
 
-alias dhr-hs='dhr -v $HOME/.stack:$HOME/.stack -v $HOME/.cabal:$HOME/.cabal -v $HOME/.ghci:$HOME/.ghci dandart/haskell'
+alias dhr-hs='dhr -v $HOME/.stack:/root/.stack -v $HOME/.cabal:/root/.cabal -v $HOME/.ghci:/root/.ghci -v $HOME/.hoogle:/root/.hoogle -v /var/run/dbus/system_bus_socket:/var/run/dbus/system_bus_socket dandart/haskell'
 
 alias cabal='dhr-hs cabal'
 alias stack='dhr-hs stack'
@@ -76,12 +175,13 @@ alias ghci='dhr-hs ghci'
 alias ghc='dhr-hs ghc'
 alias runghc='dhr-hs runghc'
 alias runhaskell='dhr-hs runhaskell'
+alias ghcid='stack exec ghcid -- -r -c stack ghci'
 
-alias dhrr-nmap='dhrr jess/nmap'
-alias nmap='dhrr-nmap'
+alias dhr-nmap='sdhr jess/nmap'
+alias nmap='dhr-nmap'
 
-alias dhrr-masscan='dhrr jess/masscan'
-alias masscan='dhrr-masscan'
+alias dhr-masscan='sdhr jess/masscan'
+alias masscan='dhr-masscan'
 
 # can't find
 # gladish rosegarden yoshimi jack-rack ardour
@@ -102,20 +202,20 @@ alias qjackctl='dhr-qjackctl qjackctl'
 #alias dhr-audacity='dhrx jess/audacity'
 #alias audacity='dhr-audacity audacity'
 
-alias dhr-wine='dhrx -v $HOME/.wine:$HOME/.wine -v $HOME/.local/share:$HOME/.local/share jess/wine'
+alias dhr-wine='dhrx -v $HOME/.wine:/root/.wine -v $HOME/.local/share:/root/.local/share jess/wine'
 alias wine='dhr-wine wine'
 
 alias dhr-torbrowser='dhrx jess/tor-browser'
 alias tor-browser='dhr-torbrowser'
 
-alias dhr-wireshark='dhrrx jess/wireshark'
+alias dhr-wireshark='sdhrx jess/wireshark'
 alias wireshark='dhr-wireshark'
 
 alias dhr-wargames='dhrx jess/wargames'
 alias wargames='dhr-wargames'
 
-alias dhr-vscode='dhrrx jess/vscode' # user "user" - can't override?
-alias vscode='dhr-vscode'
+alias dhr-vscode='sdhrrx -v $HOME/.config/Code:/home/user/.config/Code jess/vscode' # user "user" - can't override?
+alias code='dhr-vscode'
 
 alias dhr-vlc='dhrx jess/vlc'
 alias vlc='dhr-vlc'
@@ -129,7 +229,7 @@ alias traceroute='dhr-traceroute'
 alias dhr-spotify='dhrx jess/spotify'
 alias spotify='dhr-spotify'
 
-alias dhr-skype='dhrx -v $HOME/.config:$HOME/.config jess/skype'
+alias dhr-skype='dhrx -v $HOME/.config:/root/.config jess/skype'
 alias skype='dhr-skype'
 
 alias dhr-rdesktop='dhrx jess/rdesktop'
@@ -147,15 +247,24 @@ alias john='dhr-john'
 alias dhr-inkscape='dhrx jess/inkscape'
 alias inkscape='dhr-inkscape'
 
-alias dhr-cathode='dhrx -v $HOME:$HOME -v $PWD:$PWD jess/cathode'
+alias dhr-cathode='dhrx -v $HOME:$HOME -v $PWD:$PWD --device /dev/dri jess/cathode'
 alias cathode='dhr-cathode'
 
-alias dhr-kali='dhrr jess/kalilinux'
+alias dhr-kali='sdhr jess/kalilinux'
 alias kali='dhr-kali bash'
 
-alias dhr-steam='dhrx -v $HOME/.steam:$HOME/.steam -v $HOME/.local:$HOME/.local tianon/steam'
+alias dhr-steam='sdhrx -v $HOME/.steam:$HOME/.steam -v $HOME/.local:$HOME/.local tianon/steam'
 alias steam='dhr-steam'
 
 alias dhr-mongo='drri -p27017:27017 mongo'
 alias mongod='dhr-mongo'
 # alias dhr-kvm='dhrx jess/kvm'
+
+# sdhrx because ipc host
+alias dhr-firefox='sdhrx -v $HOME/.mozilla:$HOME/.mozilla -v $HOME/.cache:$HOME/.cache -v $HOME/Downloads:$HOME/Downloads -v /dev/snd:/dev/snd --device /dev/snd --device /dev/dri --device /dev/video0 --device /dev/video1 --ipc host --group-add audio --group-add video jess/firefox'
+alias fx='dhr-firefox'
+alias firefox='dhr-firefox'
+alias dfirefox='dhr-firefox'
+
+alias dhr-vim='dhr -v $HOME/.vimrc:/root/.vimrc -v /root/.vim:/root/.vim -v $HOME/.viminfo:/root/.viminfo thinca/vim'
+alias dvim='dhr-vim'
