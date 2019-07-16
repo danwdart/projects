9227e9f3735f399945a6183418c744d24d5ca2d1
Dan Dart <git@dandart.co.uk>
2019-07-16 07:42:32 +0100

    fix random

--- haskell/src/random.hs
+++ haskell/src/random.hs
@@ -1,7 +1,11 @@
 import Control.Monad
 import System.Random
 
+aa :: IO [Int]
+aa = replicateM 3 $ randomRIO (0, 255)
+
 main :: IO ()
 main = do
-    print $ replicateM 3 $ randomRIO (0, 255)
+    a <- aa
+    print a 
     -- replicateM 20 $ randomRIO ('a','z')
