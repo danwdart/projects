4a3a2e045fa378466ef574ce70d3eb61a208f0d2
Dan Dart <git@dandart.co.uk>
2019-07-16 07:43:18 +0100

    Implementing a monad

---
+++ haskell/src/monad.hs
@@ -0,0 +1,14 @@
+newtype Bob a = Bob a deriving (Show)
+
+instance Functor Bob where
+    fmap f (Bob x) = Bob (f x)
+
+instance Applicative Bob where
+    pure = Bob
+    (Bob f) <*> (Bob x) = Bob (f x)
+
+instance Monad Bob where
+    (Bob x) >>= f = f x
+
+main :: IO ()
+main = print $ Bob 2 >>= return . (*2)
