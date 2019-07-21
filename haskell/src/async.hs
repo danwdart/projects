980aeb973586a88bfb0db804dae1e99901983de4
Dan Dart <git@dandart.co.uk>
2019-07-21 13:51:09 +0100

    combine async examples

--- haskell/src/async.hs
+++ haskell/src/async.hs
@@ -5,22 +5,11 @@ task :: IO ()
 task = threadDelay 1000000
 
 main :: IO ()
-main = async task >>= wait
-
-main2 :: IO ()
-main2 = withAsync task wait
-
-main3 :: IO ((), ())
-main3 = concurrently task task
-
-task2 :: IO String
-task2 = return "Hi!"
-
-main4 :: IO ()
-main4 = mapConcurrently_ putStrLn ["Hi", "Hi2"]
-
-main5 :: IO ()
-main5 = forConcurrently_ ["1","2","3"] putStrLn
-
-main6 :: IO ()
-main6 = replicateConcurrently_ 16 (putStrLn "Hi")
+main = do
+    a <- async task
+    wait a
+    withAsync task wait
+    concurrently task task
+    mapConcurrently_ putStrLn ["Hi", "Hi2"]
+    forConcurrently_ ["1","2","3"] putStrLn
+    replicateConcurrently_ 16 (putStrLn "Hi")
