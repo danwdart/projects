d7dcb9f0124202e31df78be10f4f406bc2ee021e
Dan Dart <git@dandart.co.uk>
2019-07-28 16:11:36 +0100

    translator glitch has bad library

--- haskell/src/translatorglitch.hs
+++ haskell/src/translatorglitch.hs
@@ -3,14 +3,24 @@
 import Control.Monad
 import Control.Lens
 import Data.Function
+import Data.Text
 import Network.Google
 import Network.Google.Translate
+import System.Environment
+import System.IO
 
 main :: IO ()
-main = undefined
+main = do
+    lgr <- newLogger Trace stdout
+    setEnv "GOOGLE_APPLICATION_CREDENTIALS" "./google.json"
+    env <- newEnv <&> (envLogger .~ lgr) . (envScopes .~ cloudTranslationScope)
+    putStrLn "Fine"
+    a <- runResourceT . runGoogle env $ send myR
+    print $ a
+    print $ a^.tlrTranslations
 
 myT :: TranslateTextRequest
-myT = translateTextRequest & ttrFormat .~ Just "text" & ttrQ .~ ["ts um ik im ik an"] & ttrSource .~ (Just "so") & ttrTarget .~ (Just "en")
+myT = translateTextRequest & ttrFormat .~ Just "text" & ttrQ .~ ["hallo"] & ttrSource .~ (Just "de") & ttrTarget .~ (Just "en") & ttrModel .~ (Just "nmt")
 
 myR :: TranslationsTranslate
 myR = myT & translationsTranslate & ttPp .~ True
