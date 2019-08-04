3a2316facf7397b7d0b56dabedc7fe069253e561
Dan Dart <git@dandart.co.uk>
2019-08-04 20:43:56 +0100

    break everything

--- haskell/src/breakeverything.hs
+++ haskell/src/breakeverything.hs
@@ -1,16 +1,15 @@
-import Prelude hiding ((+), (*), (/), div)
+{-# LANGUAGE NoImplicitPrelude #-}
 
-(+), (*) :: (Num a, Show a) => a -> a -> a
+-- (+), (*) :: (Num a, Show a) => a -> a -> a
+-- a + b = (show a) ++ (show b)
+-- a * b = (show [a]) ++ (show [b])
 
-a + b = (show a) ++ (show b)
-a * b = (show [a]) ++ (show [b])
+-- (/), div :: (Num a, Show a) => a -> a -> Maybe a
 
-(/), div :: (Num a, Show a) => a -> a -> Maybe a
-
-a / b = Just $ (show a) ++ "/" ++ (show b)
-div a b = Just $ (show a) ++ " div " ++ (show b)
+--a / b = Just $ (show a) ++ "/" ++ (show b)
+-- div a b = Just $ (show a) ++ " div " ++ (show b)
 
 main :: IO ()
 main = do
     putStrLn "I broke it."
-    print $ 5 * 3
+    -- print $ 5 * 3
