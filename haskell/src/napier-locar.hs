6de0a31cd84c3bfc5416f14c9714274fcfcd7a4f
Dan Dart <git@dandart.co.uk>
2019-08-23 23:10:20 +0100

    napier locar

---
+++ haskell/src/napier-locar.hs
@@ -0,0 +1,30 @@
+import Data.Digits (digitsRev)
+import Data.Function ((&))
+import Data.Functor ((<&>))
+import Data.List (findIndex, reverse)
+import Data.Maybe (Maybe (..), catMaybes)
+
+napierSymbols :: String
+napierSymbols = (['a'..'z'] ++ ['A'..'Z'])
+
+newtype Locar = Locar { getNumber :: Int }
+
+toMaybe :: a -> Bool -> Maybe a
+toMaybe l b =
+    if b
+    then Just l
+    else Nothing
+
+-- todo filter by list instead of that monstrosity
+
+instance Show Locar where
+    show (Locar n) = digitsRev 2 n <&> (1==) & zipWith toMaybe napierSymbols & catMaybes & reverse
+
+fromString :: String -> Locar
+fromString a = a & filter (`elem` napierSymbols) <&> (\k -> findIndex (==k) napierSymbols) & catMaybes <&> (2^) & sum & Locar
+
+symToInt :: String -> Int
+symToInt = getNumber . fromString
+
+main :: IO ()
+main = undefined
