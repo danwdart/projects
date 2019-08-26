1b13af80cca642186e93582784d81b715511d0de
Dan Dart <git@dandart.co.uk>
2019-08-26 19:52:19 +0100

    hmm

--- haskell/src/uefi/efimain.c
+++ haskell/src/uefi/efimain.c
@@ -2,9 +2,8 @@
 #include <efilib.h>
 #include <HsFFI.h>
 #ifdef __GLASGOW_HASKELL__
-#include "Safe_stub.h"
+#include "main_stub.h"
 #endif
-#include <stdio.h>
  
 EFI_STATUS
 EFIAPI
@@ -12,7 +11,7 @@ efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
 {
   InitializeLib(ImageHandle, SystemTable);
   Print(L"Hello, world!\n");
-  hs_init(0,NULL);
+  hs_init(0,0);
   main();
   hs_exit();
   return EFI_SUCCESS;
