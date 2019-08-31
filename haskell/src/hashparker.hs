963dd203874399e621500c0bdb1eb4d4a8c75f1d
Dan Dart <git@dandart.co.uk>
2019-09-01 00:23:21 +0100

    Hash Parker!

--- haskell/src/hashparker.hs
+++ haskell/src/hashparker.hs
@@ -1,1 +1,7 @@
-main = undefined
+import Data.List
+
+hash :: Double -> Int
+hash = read . sort . take 6 . filter (/='0') . drop 1 . dropWhile (/='.')  . show . (** 0.03125)
+
+main :: IO ()
+main = print $ map (floor . fst) . filter ((==234477) . snd) $ map (\x -> (x, hash x)) [2..1000000]
