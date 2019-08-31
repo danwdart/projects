2578388d183f4da21f18f569c4bad6b358697992
Dan Dart <git@dandart.co.uk>
2019-09-01 00:22:10 +0100

    json stuff

--- haskell/src/json.hs
+++ haskell/src/json.hs
@@ -1,20 +1,31 @@
-{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DeriveAnyClass, DeriveGeneric, OverloadedStrings #-}
 
-import Data.ByteString.Lazy.Internal
-import Data.Aeson
+import Control.Exception
+import qualified Data.ByteString as BS
+import Data.Aeson ((.=))
+import qualified Data.Aeson as A
 import GHC.Generics
 
+-- Automatic
 data Person = Person {
       name :: String
     , age  :: Int
-    } deriving (Generic, Show)
+    } deriving (A.FromJSON ,A.ToJSON, Generic, Show)
 
-instance ToJSON Person where
-    toEncoding = genericToEncoding defaultOptions
-instance FromJSON Person
+myPerson = Person {
+    name = "Joe",
+    age = 42
+}
+
+-- Manual
+myVal :: A.Value
+myVal = A.Object ("bob" .= A.String "Ted")
 
 main = do
-    putStrLn "TODO"
-    -- putStrLn $ encode (Person {name = "Joe", age = 12})
-    -- print (decode "{\"name\":\"Joe\",\"age\":12}" :: Maybe Person)
-         
+    print $ A.encode myPerson
+    print (A.decode "{\"name\":\"Bob\",\"age\":28}" :: Maybe Person)
+    print (A.decode "{\"name\":\"Bobby Jimbo\",\"age\":28,\"location\":\"Elsewhere\"}" :: Maybe Person)
+    print (A.decode "{\"name\":\"Bobby Jimbo\"}" :: Maybe Person)
+    print $ A.encode myVal
+    print (A.decode "{\"Arbitrary\":{\"Object\":286}}" :: Maybe A.Value)
+    print (A.decode "{\"Arbitrary\":{\"Object\":286}}" :: Maybe ())
