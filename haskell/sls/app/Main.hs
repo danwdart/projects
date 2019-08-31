0ac5ae2f660e3369fdf9bfec454b18ee3279a2c0
Dan Dart <git@dandart.co.uk>
2019-09-01 00:21:28 +0100

    sls

---
+++ haskell/sls/app/Main.hs
@@ -0,0 +1,27 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+
+import AWSLambda
+import Data.Function
+import Data.Functor
+import Faker
+import Faker.TvShow.StarTrek
+import qualified Data.Text as T
+import Data.Text (Text)
+import Lib
+import qualified Data.Aeson as A
+
+gimme :: Fake a -> IO a
+gimme = generateWithSettings (setNonDeterministic defaultFakerSettings)
+
+main :: IO ()
+main = lambdaMain handler
+
+handler :: A.Value -> IO String
+handler evt = do
+  str <- character <&> (`T.append` " is awesome") & gimme <&> T.unpack
+  putStrLn str
+  someFunc
+  print evt
+  return str
