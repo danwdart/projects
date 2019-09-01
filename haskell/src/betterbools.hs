730ab1c69dad0b37dc6cf5c07983d5692b9a96fc
Dan Dart <git@dandart.co.uk>
2019-09-01 17:47:06 +0100

    Better Bools

--- haskell/src/betterbools.hs
+++ haskell/src/betterbools.hs
@@ -1,2 +1,20 @@
+{-# LANGUAGE GeneralisedNewtypeDeriving #-}
+
+import Data.Coerce
+import Unsafe.Coerce
+
+newtype LightState = LightState Bool
+
+data Lights = Off | On deriving (Enum, Eq, Ord, Show)
+
+coerceEnum :: (Enum a, Enum b) => a -> b
+coerceEnum = toEnum . fromEnum
+
 main :: IO ()
-main = undefined
+main = do
+    print (coerce $ LightState True :: Bool)
+    print (unsafeCoerce $ Off :: Bool)
+    print (fromEnum On :: Int)
+    print (toEnum 0 :: Lights)
+    print (toEnum (fromEnum Off) :: Bool)
+    print (coerceEnum On :: Bool)
