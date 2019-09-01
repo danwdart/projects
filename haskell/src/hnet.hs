4c3b1160207aab3b4c5942f5a52c9075dd105888
Dan Dart <git@dandart.co.uk>
2019-09-01 23:29:29 +0100

    HaskellNet skel

---
+++ haskell/src/hnet.hs
@@ -0,0 +1,9 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Network.HaskellNet.Auth
+import Network.HaskellNet.SMTP
+import qualified Data.Text.Lazy as T
+
+main :: IO ()
+main = doSMTP "aspmx.l.gmail.com" $ \conn ->
+   sendPlainTextMail "tohnet@dandart.co.uk" "fromhnet@dandart.co.uk" "Greetings" "Greetings!!" conn
