27efd0d23d77216eed769e9602afdd89faa17036
Dan Dart <git@dandart.co.uk>
2019-09-09 22:00:39 +0100

    correct location and host for gmail

--- haskell/src/hnet.hs
+++
@@ -1,9 +0,0 @@
-{-# LANGUAGE OverloadedStrings #-}
-
-import Network.HaskellNet.Auth
-import Network.HaskellNet.SMTP
-import qualified Data.Text.Lazy as T
-
-main :: IO ()
-main = doSMTP "aspmx.l.google.com" $ \conn ->
-   sendPlainTextMail "tohnet@dandart.co.uk" "fromhnet@dandart.co.uk" "Greetings" "Greetings!!" conn
