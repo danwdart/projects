71d90b01f41eca9a3cbe2a5f274cb193b665b99f
Dan Dart <git@dandart.co.uk>
2019-09-14 21:42:35 +0100

    update dubloons things

--- haskell/dubloons/src/server.hs
+++ haskell/dubloons/src/server.hs
@@ -1,2 +1,23 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Control.Monad.IO.Class
+import Data.ByteString.Lazy.Char8 (ByteString)
+import qualified Data.ByteString.Lazy.Char8 as BSL
+import Network.HTTP.Types
+import Network.Wai
+import Network.Wai.Handler.Warp
+
+app :: Application
+app req res = do
+    let host = remoteHost req
+    let method = requestMethod req
+    let path = pathInfo req
+    res $ responseLBS status200 [] $ "Hi, "
+        `BSL.append` (BSL.pack (show host))
+        `BSL.append` " - "
+        `BSL.append` BSL.fromChunks [method]
+        `BSL.append` " - "
+        `BSL.append` (BSL.pack (show path))
+
 main :: IO ()
-main = putStrLn "Server"
+main = run 3000 app
