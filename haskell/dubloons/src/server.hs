f6ebe4afb364001ab04140176f9b4b5fb5dbd30e
Dan Dart <git@dandart.co.uk>
2019-09-15 00:22:08 +0100

    dubloons headers detection

--- haskell/dubloons/src/server.hs
+++ haskell/dubloons/src/server.hs
@@ -1,7 +1,8 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import Control.Monad.IO.Class
-import Data.ByteString.Lazy.Char8 (ByteString)
+import Data.Aeson
+import qualified Data.ByteString.Char8 as BS
 import qualified Data.ByteString.Lazy.Char8 as BSL
 import Network.HTTP.Types
 import Network.Wai
@@ -12,12 +13,18 @@ app req res = do
     let host = remoteHost req
     let method = requestMethod req
     let path = pathInfo req
-    res $ responseLBS status200 [] $ "Hi, "
-        `BSL.append` (BSL.pack (show host))
-        `BSL.append` " - "
-        `BSL.append` BSL.fromChunks [method]
-        `BSL.append` " - "
-        `BSL.append` (BSL.pack (show path))
+    let headers = requestHeaders req
+    liftIO . print $ req
+    body <- getRequestBodyChunk req
+    let mAccept = lookup "Accept" headers
+    let mCT = lookup "Content-Type" headers
+    let resp = maybe "Hello World!" id $ do
+        accept <- mAccept
+        ct <- mCT
+        return $ accept `BS.append` ct
+    
+
+    res $ responseLBS status200 [] $ BSL.fromChunks [resp]
 
 main :: IO ()
 main = run 3000 app
