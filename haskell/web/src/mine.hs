a75493e7c7e8f916169ee18b883021044a3ed1de
Dan Dart <git@dandart.co.uk>
2019-09-24 18:37:38 +0100

    logging, confirming, prompting, drawing etc

--- haskell/web/src/mine.hs
+++ haskell/web/src/mine.hs
@@ -1,22 +1,29 @@
+import Control.Monad
 import Control.Monad.IO.Class (MonadIO(..))
 import Control.Concurrent.MVar (takeMVar, putMVar, newEmptyMVar)
 
+import Data.Function ((&))
+import Data.Functor ((<&>))
+import Data.Maybe
+
 import GHCJS.DOM
-import GHCJS.DOM.Types
+import GHCJS.DOM.AudioContext
+import GHCJS.DOM.CanvasPath
+import GHCJS.DOM.CanvasRenderingContext2D
 import GHCJS.DOM.Document
 import GHCJS.DOM.Element
-import GHCJS.DOM.Node
 import GHCJS.DOM.EventM
 import GHCJS.DOM.GlobalEventHandlers
-import GHCJS.DOM.HTMLHyperlinkElementUtils
+import GHCJS.DOM.HTMLCanvasElement
+import GHCJS.DOM.HTMLHyperlinkElementUtils  
+import GHCJS.DOM.Node
+import GHCJS.DOM.Types
+import GHCJS.DOM.WebGL2RenderingContext
 
+import Language.Javascript.JSaddle.Debug
+import Language.Javascript.JSaddle.Object
 import Language.Javascript.JSaddle.Warp
 
--- run:
--- > Language.Javascript.JSaddle.Warp.debug 3708 HelloMain.helloMain
--- Use `runOnAll` to run JavaScript on all the connected browsers. 
--- For instance the following will print the HTML from the body of each connected browser:
--- > Language.Javascript.JSaddle.Debug.runOnAll_ $ currentDocumentUnchecked >>= getBodyUnchecked >>= getInnerHTML >>= liftIO . putStrLn
 helloMain :: JSM ()
 helloMain = do
     doc <- currentDocumentUnchecked
@@ -49,7 +56,62 @@ helloMain = do
 
     -- Wait until the user clicks exit.
     liftIO $ takeMVar exitMVar
-    setInnerHTML body "<h1>Ka kite ano (See you later)</h1>"
+    setInnerHTML body "<h1>You clicked me.</h1>"
 
 main :: IO ()
 main = debug 5000 helloMain
+
+serve :: IO ()
+serve = debug 5000 (return ())
+
+log :: JSM ()
+log = void $ jsg "console" # "log" $ ["Hi folks!"]
+
+logOnAll :: IO ()
+logOnAll = runOnAll_ $ jsg "console" # "log" $ ["Hi folks!"]
+
+-- Prints Maybe Bool properly - but there's no answer if not that
+getConfirmFromClient :: JSM Bool
+getConfirmFromClient = do
+    info <- jsg1 "confirm" ["Are you sure?"]
+    v <- fromJSVal $ info
+    return . fromJust $ v
+
+-- Prints Maybe String but includes Maybe "null" - JSNull should be Nothing?
+getPromptFromClient :: JSM (Maybe String)
+getPromptFromClient = do
+    info <- jsg1 "prompt" ["What is your name"]
+    fromJSVal $ info -- no need to return because returns anyway
+
+newContextFromNewCanvas :: JSM CanvasRenderingContext2D
+newContextFromNewCanvas = do
+    d <- currentDocumentUnchecked 
+    b <- getBodyUnchecked d
+    c <- createElement d "canvas"
+    setAttribute c "height" "800px"
+    setAttribute c "width" "800px"
+    canNode <- appendChild b c
+    let canEl = uncheckedCastTo HTMLCanvasElement canNode
+    mCtx <- getContext canEl "2d" [""]
+    let gCtx = fromJust mCtx
+    return . uncheckedCastTo CanvasRenderingContext2D $ gCtx
+
+line :: Double -> Double -> Double -> Double -> CanvasRenderingContext2D -> JSM ()
+line x1 y1 x2 y2 ctx = do
+    beginPath ctx
+    moveTo ctx x1 y1
+    lineTo ctx x2 y2
+    stroke ctx
+
+drawOnNewCanvas :: JSM ()
+drawOnNewCanvas = do
+    ctx <- newContextFromNewCanvas
+    setStrokeStyle ctx "1px solid black"
+    line 100 100 700 700 ctx
+    line 100 700 700 100 ctx
+    
+
+-- getCtx :: JSM Context
+
+getBodies :: JSM String
+getBodies = currentDocumentUnchecked >>= getBodyUnchecked >>= getInnerHTML <&> fromJSString
