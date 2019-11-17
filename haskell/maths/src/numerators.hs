8691b3ebcd4560f1eeae9cefccc017becce29256
Dan Dart <git@dandart.co.uk>
2019-11-17 23:37:14 +0000

    numerators

---
+++ haskell/maths/src/numerators.hs
@@ -0,0 +1,21 @@
+import Data.Ratio
+
+toNums :: Int -> [Int] -> [Int]
+toNums a = fmap (numerator . (% a))
+
+toNumList :: Int -> [Int]
+toNumList a = toNums a [1..(a-1)]
+
+-- A112544
+bigList :: [Int]
+bigList = concat $ toNumList <$> [2..20]
+
+triangleList :: String
+triangleList = unlines $ fmap show $ toNumList <$> [2..40]
+
+main :: IO ()
+main = putStrLn triangleList
+
+-- Mine
+sumList :: [Int]
+sumList = sum . toNumList <$> [2..200]
