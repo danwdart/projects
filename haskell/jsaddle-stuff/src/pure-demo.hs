f56191a80ddb7d71bc450e7abc5354f38e208c4a
Dan Dart <git@dandart.co.uk>
2019-11-18 20:47:29 +0000

    pure demo

---
+++ haskell/jsaddle-stuff/src/pure-demo.hs
@@ -0,0 +1,40 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import qualified Data.ByteString.Lazy.Char8 as BSL
+
+import JSDOM
+import JSDOM.Document
+import JSDOM.Element
+import JSDOM.Window
+
+import Language.Javascript.JSaddle hiding ((!))
+import Language.Javascript.JSaddle.Warp
+
+import Text.Blaze.Html.Renderer.Utf8
+import Text.Blaze.Html5 as H hiding (main)
+import qualified Text.Blaze.Html5 as H (main)
+import Text.Blaze.Html5.Attributes as A
+
+page :: Html
+page = docTypeHtml ! lang "en-GB" $ do
+    H.head $
+        H.title "FRP Demo"
+    body $ do
+        header $
+            h1 "FRP Demo"
+        H.main $ do
+            p "Hi"
+            H.form $ do
+                input ! A.type_ "text" ! A.name "text"
+                input ! A.type_ "submit"
+        footer $
+            small "Made by JolHarg"
+
+jsMain :: JSM ()
+jsMain = do
+    doc <- currentDocumentUnchecked
+    elBody <- getBodyUnchecked doc
+    setInnerHTML elBody $ BSL.unpack $ renderHtml page
+    
+main :: IO ()
+main = run 5000 jsMain
