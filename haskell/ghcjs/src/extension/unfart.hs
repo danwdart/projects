63c8a050beadadfb61fd30dc9cc699e0c504f9a5
Dan Dart <git@dandart.co.uk>
2019-11-18 18:54:43 +0000

    split web other to ghcjs only stuff and jsaddle stuff

---
+++ haskell/ghcjs/src/extension/unfart.hs
@@ -0,0 +1,55 @@
+{-# LANGUAGE JavaScriptFFI, OverloadedStrings #-}
+
+{-
+const doReplaceName = replacements.forEach(
+    (v, k) => document.documentElement.innerHTML = document.documentElement.innerHTML.replace(new RegExp(k, 'g'), v));
+const doReplaceTweets = twitterUr; === window.location.href ?
+    [].forEach.call(document.querySelectorAll('.TweetTextSize'), p => p.innerHTML = tr) :
+    () => {};
+-}
+
+import Data.JSString ()
+import GHCJS.Types
+import Data.Map (Map)
+import qualified Data.Map as Map
+-- import qualified JavaScript.Web.
+
+
+--foreign import javascript unsafe "console.log($1)" js_log :: JSString -> IO ()
+
+twitterUrl :: String
+twitterUrl = "https://twitter.com/realdonaldtrump"
+
+tweetReplacements :: String
+tweetReplacements = "Pfffffrt"
+
+tweetLocations :: String
+tweetLocations = ".TweetTextSize"
+
+replacements :: Map String String
+replacements = Map.fromList
+    [("Donald John Trump", "Arsehole Rectum Fart")
+    ,("Donald J. Trump", "Arsehole R. Fart")
+    ,("Donald Trump", "Arsehole Fart")
+    ,("Trump", "Fart")
+    ]
+
+replaceNames :: IO ()
+replaceNames = undefined
+
+replaceTweets :: IO ()
+replaceTweets = undefined
+
+performReplacements :: IO ()
+performReplacements = do
+    replaceNames
+    replaceTweets
+
+onLoad :: IO () -> IO ()
+onLoad action = undefined
+
+main :: IO ()
+main =
+    --js_log "Unfart initialised"
+    onLoad performReplacements
+    --js_log "Unfart finished"
---
+++ haskell/web-other/src/something.hs
@@ -0,0 +1,15 @@
+import GHCJS.DOM
+import GHCJS.DOM.Document
+import GHCJS.DOM.Element
+import GHCJS.DOM.Node
+import GHCJS.DOM.Types
+
+import Language.Javascript.JSaddle.Object
+import Language.Javascript.JSaddle.Warp
+
+main :: IO ()
+main = run 5000 jsMain
+
+jsMain :: JSM()
+jsMain = do
+    return ()
