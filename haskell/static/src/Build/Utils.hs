98d8e8e64d4aa949a29fe889cede8036dc27ebf5
Dan Dart <git@dandart.co.uk>
2020-01-07 00:06:03 +0000

    delete out static

--- haskell/static/src/Build/Utils.hs
+++
@@ -1,6 +0,0 @@
-module Build.Utils (mkdirp) where
-
-import System.Directory
-
-mkdirp :: String -> IO ()
-mkdirp = createDirectoryIfMissing True
--- haskell/static/src/Html/Common/Utils.hs
+++
@@ -1,6 +0,0 @@
-module Html.Common.Utils (intercalateAttr) where
-
-import Text.Blaze.Html5 as H hiding (main)
-
-intercalateAttr :: AttributeValue -> [AttributeValue] -> AttributeValue
-intercalateAttr x = foldl1 (\acc y -> acc <> x <> y)
