08d8c9c2e761d8df83b34264ea60c3984b054eb0
Dan Dart <git@dandart.co.uk>
2020-01-23 22:54:05 +0000

    is that how you start a parser

--- haskell/other/src/parser.hs
+++ haskell/other/src/parser.hs
@@ -1,4 +1,24 @@
+{-# LANGUAGE DeriveAnyClass, DeriveFunctor, GeneralisedNewtypeDeriving #-}
+
+import Data.Char
 -- import Text.Parsec
 
 main :: IO ()
 main = return ()
+
+type ParseResult a = (Maybe a, String)
+
+newtype Parser a = Parser {
+    runParser :: String -> ParseResult a
+} deriving (Functor, Applicative)
+
+matchCharFn :: (Char -> Bool) -> String -> ParseResult Char
+matchCharFn fn (x:xs)
+    | fn x = (Just x, xs)
+    | otherwise = (Nothing, xs)
+
+matchChar :: Char -> String -> ParseResult Char
+matchChar = matchCharFn . (==)
+
+matchAlpha :: String -> ParseResult Char
+matchAlpha = matchCharFn isAlpha
