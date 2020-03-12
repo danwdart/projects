27e12267dfd8251ea249a553cc489435384196b2
Dan Dart <git@dandart.co.uk>
2020-03-12 16:45:59 +0000

    rws demo

--- haskell/other/src/rws.hs
+++ haskell/other/src/rws.hs
@@ -1,4 +1,21 @@
--- import Control.Monad.RWS
+import Control.Monad.IO.Class
+import qualified Control.Monad.RWS as RWS
+import qualified Control.Monad.Trans.RWS as RWST
+
+stuff :: RWS.RWS String [String] Int Char
+stuff = do
+    RWS.tell ["hi"]
+    RWS.put 2
+    RWS.asks (!! 0)
+
+stuff2 :: RWST.RWST String [String] Int IO Char
+stuff2 = do
+    RWST.tell ["hey"]
+    liftIO . print $ "AAA"
+    RWST.put 2
+    RWST.asks (!! 0)
 
 main :: IO ()
-main = undefined
+main = do
+    print $ RWS.runRWS stuff "aeiou" 1
+    print =<< RWST.runRWST stuff2 "aeiou" 1
