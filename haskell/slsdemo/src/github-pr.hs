fbeee15dc57de6a9b0fd6e1a83bf704b9dac29c4
Dan Dart <git@dandart.co.uk>
2020-03-14 20:40:12 +0000

    api gateway working

--- haskell/slsdemo/src/github-pr.hs
+++ haskell/slsdemo/src/github-pr.hs
@@ -1,10 +1,19 @@
+{-# LANGUAGE DeriveAnyClass, DeriveGeneric #-}
 import AWSLambda
+import AWSLambda.Events.APIGateway
+import Control.Lens
 import Data.Aeson
+import Data.Aeson.Embedded
+import Data.Map
+import GHC.Generics
 import qualified Data.ByteString.Lazy.Char8 as BSL
 -- import Data.Text
 
 main :: IO ()
-main = lambdaMain handler
+main = apiGatewayMain handler
 
-handler :: Value -> IO String
-handler evt = return $ BSL.unpack $ encode evt
+handler :: APIGatewayProxyRequest (Embedded Value) -> IO (APIGatewayProxyResponse String)
+handler request = do
+  putStrLn "This should go to logs"
+  print $ request ^. requestBody
+  pure $ responseOK & responseBody ?~ "{\"Airse\":\"Baulacks\"}"
