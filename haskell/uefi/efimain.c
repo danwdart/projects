fc0c85060eecbc13f506e0dafe1be5d63e38221e
Dan Dart <git@dandart.co.uk>
2020-04-12 22:45:45 +0100

    Attempts to make a bootable OS

--- haskell/uefi/efimain.c
+++ haskell/uefi/efimain.c
@@ -1,18 +1,13 @@
 #include <efi.h>
 #include <efilib.h>
-#include <HsFFI.h>
-#ifdef __GLASGOW_HASKELL__
-#include "main_stub.h"
-#endif
- 
 EFI_STATUS
 EFIAPI
 efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
 {
   InitializeLib(ImageHandle, SystemTable);
   Print(L"Hello, world!\n");
-  hs_init(0,0);
-  main();
-  hs_exit();
+  // hs_init(0,0);
+  // main();
+  // hs_exit();
   return EFI_SUCCESS;
 }
