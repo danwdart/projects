cdd6559f9cb09de9a9da00b243278a827b6ee5f0
Dan Dart <git@dandart.co.uk>
2020-04-14 00:12:29 +0100

    frog

---
+++ haskell/boardgames/src/frog.hs
@@ -0,0 +1,18 @@
+import Control.Monad
+import System.Random
+
+main :: IO ()
+main = print =<< avg <$> replicateM 1000000 (randomJump 1 10)
+
+randomJump :: Int -> Int -> IO Int
+randomJump js total = do
+    a <- randomRIO (1, total) :: IO Int
+    if a < total
+    then randomJump (js + 1) (total - a)
+    else return js
+
+freqList :: [Int] -> [(Int, Int)]
+freqList = undefined
+
+avg :: [Int] -> Double
+avg xs = fromIntegral (sum xs) / fromIntegral (length xs)
