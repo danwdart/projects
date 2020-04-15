f9cf30c705a01b529fc078745352f49a623834b4
Dan Dart <git@dandart.co.uk>
2020-04-15 19:39:50 +0100

    van eck

---
+++ haskell/maths/src/vaneck.hs
@@ -0,0 +1,10 @@
+import Data.List
+import Data.Maybe
+
+main :: IO ()
+main = print $ iterate iter [0] !! 151
+
+iter :: [Integer] -> [Integer]
+iter [] = [0]
+iter xs = let (most, end) = (init xs, last xs)
+    in most <> [end] ++ [maybe 0 (\x -> fromIntegral $ x + 1) (end `elemIndex` reverse most)]
