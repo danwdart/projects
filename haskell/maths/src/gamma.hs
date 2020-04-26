e3aa4e7be106d4040fa9bb6f08c61e4f30bf566a
Dan Dart <git@dandart.co.uk>
2020-04-26 23:29:42 +0100

    gamma

---
+++ haskell/maths/src/gamma.hs
@@ -0,0 +1,15 @@
+{-# LANGUAGE TypeOperators #-}
+
+import Prelude hiding (succ, (.))
+import Data.Invertible
+
+-- Not really invertible but whatever
+sqr :: (Fractional a, Floating a)=> a <-> a
+sqr = (^2) <-> sqrt
+
+gammaI :: Double <-> Double
+gammaI = involve recip . succ . involve negate . sqr
+
+gamma, unGamma :: Double -> Double
+gamma = biTo gammaI
+unGamma = biFrom gammaI
