cbd55ab50809d10debf79a53306b6fdeead8e0dd
Dan Dart <git@dandart.co.uk>
2020-05-10 14:27:50 +0100

    hgamer3d and tardis

--- haskell/other/src/tardis.hs
+++ haskell/other/src/tardis.hs
@@ -1,4 +1,9 @@
 import Control.Monad.Tardis
 
 main :: IO ()
-main = return ()
+main = print $ flip runTardis ("I'm The Doctor.", "I'm The Doctor.") $ do
+    y <- getPast
+    sendFuture "2073"
+    sendPast "1973"
+    x <- getFuture
+    return (x, y)
