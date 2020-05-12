45ad6be48bb4e6d565920f3b7fa3307ea46c5ab1
Dan Dart <git@dandart.co.uk>
2020-05-12 20:08:39 +0100

    update slsdemo

---
+++ haskell/slsdemo/src/emailme.hs
@@ -0,0 +1,30 @@
+{-# LANGUAGE OverloadedStrings #-}
+import AWSLambda
+import AWSLambda.Events.APIGateway
+import Control.Lens
+import Data.Aeson
+import Data.Aeson.Embedded
+import qualified Data.ByteString.Lazy.Char8 as BSL
+import Data.Map
+import GHC.Generics
+import Network.AWS.SES
+import Network.AWS.SES.SendRawEmail
+import System.Environment
+import qualified Data.Text as T
+
+main :: IO ()
+main = apiGatewayMain handler
+
+handler :: APIGatewayProxyRequest (Embedded Value) -> IO (APIGatewayProxyResponse (Embedded Value))
+handler request = do
+  env <- getEnvironment
+  putStrLn "This should go to logs"
+  print $ request ^. requestBody
+  let rsp = sendRawEmail (rawMessage "Hello World") &
+        sreDestinations .~ ["amazon@dandart.co.uk"] &
+            sreSource ?~ "amazon@dandart.co.uk"
+  pure $ responseOK &
+    responseBody
+    ?~
+      Embedded
+        (object $ fmap (\ (k, v) -> (T.pack k, String (T.pack v))) env)
