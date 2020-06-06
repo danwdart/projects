4b376f742466b38fc588d7d500bc7877070f4f7d
Dan Dart <git@dandart.co.uk>
2020-06-07 00:01:04 +0100

    applicative do explanation

--- haskell/other/8.10.1/src/applicativedo.hs
+++ haskell/other/8.10.1/src/applicativedo.hs
@@ -3,10 +3,20 @@
 import Data.Functor.Compose
 
 main :: IO ()
-main = print . getCompose $ c
+main = do
+    print . getCompose $ c
+    print d
 
 c :: Compose [] [] (Char, Char)
 c = do
     a <- Compose ["aa"]
     b <- Compose ["bb"]
     return (a, b)
+
+-- tbh this could work with monads too
+d :: [Int]
+d = do
+    a <- [1..5]
+    b <- [1..9]
+    return $ a + b -- (+) <$> a <*> b
+
