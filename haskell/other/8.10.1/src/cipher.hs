c2d3991af94d156b062a75b28515ccbe8b581303
Dan Dart <git@dandart.co.uk>
2020-06-08 01:41:42 +0100

    cipherer

--- haskell/other/8.10.1/src/cipher.hs
+++ haskell/other/8.10.1/src/cipher.hs
@@ -1,19 +1,25 @@
 {-# LANGUAGE DeriveFunctor #-}
 
 import Control.Applicative
+import Control.Monad
 import Data.Char
 import Data.List
 import Data.Maybe
+import System.IO
 
-consonants, vowels :: String
-consonants = "bcdfghjklmnpqrstvwxyz"
-vowels = "aeiou"
+consonants, vowels, consCaps, vowelCaps :: String
+consonants = "bcdfghjklmnpqrstvwxz"
+vowels = "aeiouy"
+consCaps = "BCDFGHJKLMNPQRSTVWXZ"
+vowelCaps = "AEIOUY"
 
-data Letter x = Vowel x | Consonant x | Other x deriving (Eq, Show, Functor)
+data Letter x = Vowel x | Consonant x | VowelCap x | ConsCap x | Other x deriving (Eq, Show, Functor)
 
 index :: Char -> Maybe (Letter Int)
-index c = fmap Consonant (c `elemIndex` consonants) <|>
-          fmap Vowel (c `elemIndex` vowels) <|>
+index c = Consonant <$> (c `elemIndex` consonants) <|>
+          Vowel <$> (c `elemIndex` vowels) <|>
+          VowelCap <$> (c `elemIndex` vowelCaps) <|>
+          ConsCap <$> (c `elemIndex` consCaps) <|> 
           Other <$> Just (ord c)
 
 cipherChar :: Int -> Char -> Maybe Char
@@ -25,10 +31,27 @@ modIndex i xs = xs !! (i `mod` length xs)
 toChar :: Int -> Letter Int -> Char
 toChar i (Consonant n) = modIndex (n + i) consonants
 toChar i (Vowel n) = modIndex (n + i) vowels
+toChar i (VowelCap n) = modIndex (n + i) vowelCaps
+toChar i (ConsCap n) = modIndex (n + i) consCaps
 toChar _ (Other n) = chr n
 
 cipher :: Int -> String -> String
-cipher k = mapMaybe (cipherChar k . toLower)
+cipher = mapMaybe . cipherChar
+
+cyclingCipher :: String -> String
+cyclingCipher str = do
+    ch <- zip [1..] str
+    let (Just ch2) = uncurry cipherChar ch
+    return ch2
 
 main :: IO ()
-main = putStrLn $ cipher 2 "Hi world! My name is Bob!"
+main = do
+    putStrLn $ cipher 2 "Hi world! My name is Bob!"
+    forM_ [1..10] $ \n -> do
+        putStr $ show n ++ ": "
+        putStrLn $ cipher n "Helo werld, mai neim iz bob."
+    -- TODO cycling cipher
+    putStrLn $ cyclingCipher "Aaaaaaaa, hello my friends! It is I, the Ruminating Demon!"
+    hSetEcho stdout False
+    interact cyclingCipher
+    hSetEcho stdout True
