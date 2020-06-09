9799aff7886db026d3f3aa12fa24cf23df6509a2
Dan Dart <git@dandart.co.uk>
2020-06-09 02:42:41 +0100

    asts

--- haskell/other/8.8.3/src/langparse.hs
+++ haskell/other/8.8.3/src/langparse.hs
@@ -1,8 +1,13 @@
 {-# LANGUAGE RankNTypes    #-}
 {-# LANGUAGE UnicodeSyntax #-}
 
+import Language.JavaScript.Parser
+import Language.C
+
 testFile ∷ String
 testFile = "(function main(a, b) { console.log(a + b); })(1, 2);"
 
 main ∷ IO ()
-main = print "Hello World"
+main = do
+    print $ readJs testFile
+    print =<< parseCFilePre "/usr/include/stdio.h"
