7fcb79be4eb5968e7b907dfc1dcb0448344fd896
Dan Dart <git@dandart.co.uk>
2020-09-02 23:15:35 +0100

    Update maths and version of other8101 -> other8102

---
+++ haskell/maths/src/lucas.hs
@@ -0,0 +1,20 @@
+phi :: Double
+phi = (sqrt 5 + 1) / 2
+
+iphi :: Double
+iphi = -(sqrt 5 - 1) / 2
+
+powphicaret :: Integer -> Integer
+powphicaret n = round (phi ^ n)
+
+powphiiphicaret :: Integer -> Integer
+powphiiphicaret n = round (phi ^ n - iphi ^ n)
+
+powphistarstar :: Integer -> Integer
+powphistarstar n = round (phi ** fromInteger n)
+
+powphiiphistarstar :: Integer -> Integer
+powphiiphistarstar n = round (phi ** fromInteger n - iphi ** fromInteger n)
+
+lucas :: Integer -> Integer
+lucas n = undefined
--- haskell/maths/src/surds.hs
+++ haskell/maths/src/surds.hs
@@ -1,7 +1,21 @@
-newtype Root = Root Int
+data Combination = Sum Combination Combination |
+    Product Combination Combination |
+    Plain Int |
+    Root Int
 
-instance Show Root where
+instance Show Combination where
+    show (Sum c1 c2) = "(" ++ show c1 ++ " + " ++ show c2 ++ ")"
+    show (Product c1 c2) = "(" ++ show c1 ++ show c2 ++ ")"
+    show (Plain a) = show a
     show (Root a) = "√" ++ show a
 
+instance Num Combination where
+    c1 + c2 = undefined
+    c1 * c2 = undefined
+    abs c1 = undefined
+    signum c1 = undefined
+    negate c1 = undefined
+    fromInteger = Plain . fromInteger
+
 main :: IO ()
-main = undefined
+main = print $ Sum (Product (Plain 2) (Root 2)) (Product (Plain 3) (Root 3))
