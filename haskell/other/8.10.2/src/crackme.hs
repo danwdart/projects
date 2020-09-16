adbbe2309b38a63cceffffbadc11f9a030e2f91b
Dan Dart <git@dandart.co.uk>
2020-09-16 21:58:59 +0100

    update fileformat and crackme for re

---
+++ haskell/other/8.10.2/src/crackme.hs
@@ -0,0 +1,15 @@
+import System.IO
+
+main :: IO ()
+main = do
+    putStrLn "Crack me!"
+    putStr "Enter password: "
+    hSetEcho stdin False
+    pw <- getLine
+    hSetEcho stdin True
+    putStrLn ""
+    if pw == "hello"
+    then
+        putStrLn "Hi!"
+    else
+        putStrLn "Wrong password."
--- haskell/other/8.10.2/src/fileformat.hs
+++ haskell/other/8.10.2/src/fileformat.hs
@@ -3,6 +3,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import Data.Binary
+import Data.ByteString.Char8
 import GHC.Generics
 
 data Png = Png {
@@ -14,3 +15,11 @@ main :: IO ()
 main = do
     print (decode "aaaaaaaa" :: Png)
     print . encode $ Png 10 10
+    print (decode "A\NUL\NUL\NUL\NUL\NUL\NUL\NUL\fHello World!v" :: Variable)
+    print . encode $ Variable { Main.length = 12, var = "AAAAAAAAAAAA", tree = 65 }
+
+data Variable = Variable {
+    length :: !Word8,
+    var :: !ByteString,
+    tree :: !Word8
+} deriving (Generic, Binary, Show)
