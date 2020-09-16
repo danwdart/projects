305601d3ee63700c3232b0ca473c1ed90c0fbe0c
Dan Dart <git@dandart.co.uk>
2020-09-16 13:03:31 +0100

    use binary generic

--- haskell/other/8.10.2/src/fileformat.hs
+++ haskell/other/8.10.2/src/fileformat.hs
@@ -1,15 +1,16 @@
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE OverloadedStrings #-}
 
-import           Data.Binary.Get
-import           Data.ByteString.Char8
-import           Data.Word
+import Data.Binary
+import GHC.Generics
 
 data Png = Png {
     magicNumber :: !Word32,
     version     :: !Word32
-} deriving (Show)
-
-
+} deriving (Generic, Binary, Show)
 
 main :: IO ()
-main = print (runGet (Png <$> getWord32le <*> getWord32le) "aaaaaaaa")
+main = do
+    print (decode "aaaaaaaa" :: Png)
+    print . encode $ Png 10 10
