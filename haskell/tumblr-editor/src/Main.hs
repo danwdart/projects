2f1135ea72807ea593634b965b7a289ebe0ca0dc
Dan Dart <git@dandart.co.uk>
2020-11-16 18:16:13 +0000

    tumblr editor

--- haskell/tumblr-editor/src/Main.hs
+++ haskell/tumblr-editor/src/Main.hs
@@ -6,6 +6,9 @@ module Main where
 
 import Control.Monad.IO.Class
 import Control.Concurrent
+import Data.Functor.Compose
+import Data.Text (pack, unpack)
+import System.Environment
 import           Test.WebDriver
 import           Test.WebDriver.Class
 import Test.WebDriver.Session
@@ -34,31 +37,40 @@ chromeConfig = defaultConfig {
 -- Run java -jar /usr/share/selenium-server/selenium-server-standalone.jar
 
 main :: IO ()
-main = runSession firefoxConfig $ do
-    let email = "aaaa"
-    let password = "aaa"
-    openPage "https://tumblr.com"
-    login <- findElem (ById "signup_login_button")
-    click login
-    emailBox <- findElem (ById "signup_determine_email")
-    sendKeys email emailBox
-    loginButton <- findElem (ById "signup_forms_submit")
-    click loginButton
-    passwordButton <- findElem (ById "forgot_password_link")
-    click passwordButton
-    passwordBox <- findElem (ById "signup_password")
-    sendKeys password passwordBox
-    click loginButton
-    liftIO (threadDelay 10000000)
-    {-}
-    openPage $ "http://" <> siteName <> ".localhost:" <> show myPort
-    getSession
-    as <- findElems (ByCSS "a[href^=http]")
-    hrefs <- mapM (`attr` "href") as
-    as <- findElems (ByCSS "img[src^=http]")
-    navbar <- findElem $ ByClass "navbar-nav"
-    linkName <- getText linkToClick
-    click linkToClick
-    cards <- findElems $ ByClass "card"
-    -}
-    closeSession
+main = do
+    [email, password, blog] <- getCompose $ pack <$> Compose getArgs
+    runSession firefoxConfig $ do
+        setImplicitWait 500000
+        openPage "https://tumblr.com"
+        login <- findElem (ById "signup_login_button")
+        click login
+        emailBox <- findElem (ById "signup_determine_email")
+        sendKeys email emailBox
+        loginButton <- findElem (ById "signup_forms_submit")
+        click loginButton
+        liftIO (threadDelay 500000)
+        passwordButton <- findElem (ByClass "forgot_password_link")
+        click passwordButton
+        passwordBox <- findElem (ById "signup_password")
+        sendKeys password passwordBox
+        click loginButton
+        liftIO (threadDelay 3000000)
+        agreeButton <- findElem (ByCSS "button[mode=primary]")
+        click agreeButton
+        liftIO (threadDelay 2000000)
+        openPage $ "https://tumblr.com/blog/" <> unpack blog
+        editButton <- findElem (ByCSS "[aria-label=\"Edit\"")
+        click editButton
+        liftIO (threadDelay 10000000)
+        {-}
+        openPage $ "http://" <> siteName <> ".localhost:" <> show myPort
+        getSession
+        as <- findElems (ByCSS "a[href^=http]")
+        hrefs <- mapM (`attr` "href") as
+        as <- findElems (ByCSS "img[src^=http]")
+        navbar <- findElem $ ByClass "navbar-nav"
+        linkName <- getText linkToClick
+        click linkToClick
+        cards <- findElems $ ByClass "card"
+        -}
+        closeSession
