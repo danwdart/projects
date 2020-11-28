1436b15c5ec502345c42bd6419a4503b9641a9e7
Dan Dart <git@dandart.co.uk>
2020-11-28 03:17:06 +0000

    existentialism!

--- haskell/other/8.10.2/src/existentialism.hs
+++ haskell/other/8.10.2/src/existentialism.hs
@@ -1,5 +1,46 @@
+{-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE MultiWayIf #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE UnicodeSyntax #-}
-import           Control.Monad
+
+import Control.Exception
+import Data.Typeable
+
+-- https://blog.sumtypeofway.com/posts/existential-haskell.html
+
+-- Inaccessible container for reifying a class?
+
+data Showable = forall a . Show a => Showable a
+
+showable :: Showable -> String
+showable x = case x of Showable val -> show val
+
+s :: Showable
+s = Showable "Hi!"
+
+
+data ShowableG where
+  ShowableG :: Show a => a -> ShowableG
+
+showableG :: ShowableG -> String
+showableG x = case x of ShowableG val -> show val
+
+sg :: ShowableG
+sg = ShowableG "Hi!"
+
+cautiouslyPrint :: Show a => IO a -> IO ()
+cautiouslyPrint go = Control.Exception.catch (go >>= print) handler
+  where
+    handler :: SomeException -> IO ()
+    handler (SomeException e) = if
+      | Just (arith :: ArithException) <- cast e -> putStrLn ("arith: " <> show arith)
+      | Just (array :: ArrayException) <- cast e -> putStrLn ("array: " <> show array)
+      | otherwise -> putStrLn ("Something else: " <> show e)
 
 main ∷ IO ()
-main = void $ pure Nothing
+main = do
+  putStrLn $ showable s
+  putStrLn $ showableG sg
+  cautiouslyPrint . pure $ "Everything is fine."
+  cautiouslyPrint $ (ioError . userError $ "AAAH!" :: IO String)
