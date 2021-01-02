359f7350fbea0ed7e0a4d23ce5cc3df27ce2fb77
Dan Dart <git@dandart.co.uk>
2021-01-02 20:59:28 +0000

    init shop

---
+++ haskell/other/8.10.2/src/shop.hs
@@ -0,0 +1,26 @@
+{-
+Main screen options
+
+Order mode
+Self mode
+Staff mode
+
+Order options
+Look up product
+Add product from lookup (checks stock)
+Login/register for delivery address save
+Do delivery or collection
+
+Self options
+Beep in a product
+Once more than product -> pay
+pay
+
+Staff options
+Check stock
+Order in
+
+-}
+
+main :: IO ()
+main = pure ()
