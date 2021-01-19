1950b3b5de29337ee532410465cdaf15d9ee158a
Dan Dart <git@dandart.co.uk>
2021-01-19 19:55:26 +0000

    add a nix shell demo

---
+++ haskell/cli/nix-demo.hs
@@ -0,0 +1,14 @@
+#!/usr/bin/env nix-shell
+#! nix-shell -i runghc -p haskell.compiler.ghc8102
+
+{-# LANGUAGE OverloadedLists #-}
+
+import Data.Map ( Map )
+
+g :: Map Int Int
+g = [(1,1)]
+
+main :: IO ()
+main = do
+    print g
+    putStrLn "Hello!"
