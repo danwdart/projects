38db15b4d32731d5e720671c3b88e0232a6d1cd4
Dan Dart <git@dandart.co.uk>
2021-01-31 18:53:20 +0000

    consolefrp nix?

---
+++ haskell/consolefrp/default.nix
@@ -0,0 +1,31 @@
+{ nixpkgs ? import <nixpkgs> {},
+  compiler ? "ghc865" }:
+let
+  gitignore = nixpkgs.nix-gitignore.gitignoreSourcePure [ ./.gitignore ];
+  myHaskellPackages = nixpkgs.pkgs.haskell.packages.${compiler}.override {
+    overrides = self: super: rec {
+      consolefrp = self.callCabal2nix "consolefrp" (gitignore ./.) {};
+    };
+  };
+  shell = myHaskellPackages.shellFor {
+    packages = p: [
+      p.consolefrp
+    ];
+    buildInputs = [
+      nixpkgs.haskellPackages.cabal-install
+      nixpkgs.wget
+      nixpkgs.haskellPackages.stack
+      nixpkgs.haskellPackages.ghcid
+      nixpkgs.haskellPackages.stylish-haskell
+      nixpkgs.haskellPackages.hlint
+    ];
+    # withHoogle = true;
+  };
+  exe = nixpkgs.haskell.lib.justStaticExecutables (myHaskellPackages.consolefrp);
+in
+{
+  inherit shell;
+  inherit exe;
+  inherit myHaskellPackages;
+  consolefrp = myHaskellPackages.consolefrp;
+}
