ee16aeaaf774511541fd9c569e9246691fc15a75
Dan Dart <git@dandart.co.uk>
2021-02-01 10:06:37 +0000

    some perhaps from last night

--- haskell/jsaddle-stuff/default.nix
+++ haskell/jsaddle-stuff/default.nix
@@ -3,8 +3,20 @@
   ghcjs ? "ghcjs86"}:
 let
   gitignore = nixpkgs.nix-gitignore.gitignoreSourcePure [ ./.gitignore ];
-  myHaskellPackages = nixpkgs.pkgs.haskell.packages.${compiler}.override {
+  myHaskellPackages = nixpkgs.pkgs.haskell.packages.${ghcjs}.override {
     overrides = self: super: rec {
+      entropy = self.callCabal2nix "entropy" (builtins.fetchGit {
+        url = "https://github.com/TomMD/entropy.git";
+        rev = "a43996e285e5afdfe933f160d611439581a453a0";
+      }) {};
+      network = self.callCabal2nix "network" (builtins.fetchGit {
+        url = "https://github.com/haskell/network.git";
+        ref = "v2.8.0.0";
+      }) {};
+      foundation = self.callCabal2nix "foundation" (builtins.fetchGit {
+        url = "https://github.com/haskell-foundation/foundation.git";
+        rev = "58568e9f5368170d272000ecf16ef64fb91d0732";
+      }) {};
       jsaddle-stuff = self.callCabal2nix "jsaddle-stuff" (gitignore ./.) {};
     };
   };
