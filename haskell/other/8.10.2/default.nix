a1aa878e9a54f644f2fb0408116a9d0e2b55d232
Dan Dart <git@dandart.co.uk>
2021-02-10 23:54:18 +0000

    go back down to 8.10.2 because:
    ```
    [20 of 21] Compiling Data.ListLike.Instances ( src/Data/ListLike/Instances.hs, dist/build/Data/ListLike/Instances.o, dist/build/Data/ListLike/Instances.dyn_o )
    
    src/Data/ListLike/Instances.hs:135:10: error:
        Duplicate instance declarations:
          instance IsList BSC.ByteString
            -- Defined at src/Data/ListLike/Instances.hs:135:10
          instance IsList BSC.ByteString
            -- Defined in ‘Data.ByteString.Internal’
        |
    135 | instance IsList BS.ByteString where
        |          ^^^^^^^^^^^^^^^^^^^^
    
    src/Data/ListLike/Instances.hs:258:10: error:
        Duplicate instance declarations:
          instance IsList BSLC.ByteString
            -- Defined at src/Data/ListLike/Instances.hs:258:10
          instance IsList BSLC.ByteString
            -- Defined in ‘Data.ByteString.Lazy.Internal’
        |
    258 | instance IsList BSL.ByteString where
    ```

--- haskell/other/8.10.2/default.nix
+++ haskell/other/8.10.2/default.nix
@@ -1,5 +1,5 @@
 { nixpkgs ? import <nixpkgs> {},
-  compiler ? "ghc8103" }:
+  compiler ? "ghc8102" }:
 let
   gitignore = nixpkgs.nix-gitignore.gitignoreSourcePure [ ./.gitignore ];
   myHaskellPackages = nixpkgs.pkgs.haskell.packages.${compiler}.override {
