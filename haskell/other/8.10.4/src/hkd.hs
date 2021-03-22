a4d36fb9598aa98e3fa2a83e65bb3b70d3be049f
Dan Dart <git@dandart.co.uk>
2021-03-22 18:59:51 +0000

    idk

--- haskell/other/8.10.4/src/hkd.hs
+++ haskell/other/8.10.4/src/hkd.hs
@@ -2,12 +2,13 @@
 {-# LANGUAGE DeriveGeneric        #-}
 {-# LANGUAGE FlexibleInstances    #-}
 {-# LANGUAGE StandaloneDeriving   #-}
+{-# LANGUAGE TypeApplications     #-}
 {-# LANGUAGE UndecidableInstances #-}
 {-# OPTIONS_GHC -Wwarn #-}
 
 import           Barbies
 import           Control.Applicative
--- import           Data.Functor.Identity
+import           Data.Functor.Identity
 import           GHC.Generics
 
 newtype Name = Name String deriving (Eq, Read, Show)
@@ -19,14 +20,24 @@ data Person f = Person {
     profession :: f Profession
 } deriving (Generic, FunctorB, TraversableB, ApplicativeB, ConstraintsB)
 
+deriving instance AllBF Read f Person => Read (Person f)
 deriving instance AllBF Show f Person => Show (Person f)
 deriving instance AllBF Eq   f Person => Eq   (Person f)
+
+c :: Const String Int
+c = Const "1"
+
+i :: Identity Int
+i = Identity . read . getConst $ c
 
 personRaw :: Person (Const String)
 personRaw = Person {
     name = Const "Bob",
     profession = Const "Coder"
 }
+
+personContainer :: Container Person String
+personContainer = Container personRaw
 
 main :: IO ()
 main = do
