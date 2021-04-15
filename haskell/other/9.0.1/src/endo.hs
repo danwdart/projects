b2ea91d25a6394260fa99463608849aaa285f4a4
Dan Dart <git@dandart.co.uk>
2021-04-15 01:05:22 +0100

    iso and endo examples

---
+++ haskell/other/9.0.1/src/endo.hs
@@ -0,0 +1,18 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Data.Semigroup
+import Data.Text
+
+excite :: Text -> Text
+excite = (<> "!")
+
+multiExcite :: Int -> Text -> Text
+multiExcite = flip stimes excite
+
+multiEndoExcite :: Int -> Text -> Text
+multiEndoExcite n = appEndo . stimes n $ Endo excite
+
+main :: IO ()
+main = do
+    print . multiExcite 3 $ "Hi" -- This is "Hi!Hi!Hi!"
+    print . multiEndoExcite 3 $ "Hi" -- but this is "Hi!!!"
---
+++ haskell/other/9.0.1/src/iso.hs
@@ -0,0 +1,9 @@
+import Data.Isomorphism
+
+staticIso :: Iso (,) Int String
+staticIso = Iso (1,"b") ("b",1)
+
+main :: IO ()
+main = do
+    print . embed $ staticIso
+    print . project $ staticIso
