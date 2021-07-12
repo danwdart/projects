56fb0487f87741506d1f2c780daaad77e4f01fda
Dan Dart <git@dandart.co.uk>
2021-07-12 23:39:57 +0100

    Weird instances

---
+++ haskell/other/onlybase/src/weird-instances.hs
@@ -0,0 +1,28 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# OPTIONS_GHC -Wno-orphans #-}
+
+import Data.String
+
+-- Why won't this work
+{-}
+instance IsString (String -> IO ()) where
+    fromString a b = do
+        putStrLn a
+        putStrLn b
+-}
+
+-- Whereas these will
+
+-- instance IsString (IO ()) where
+--     fromString a = putStrLn a
+
+instance (a ~ String, b ~ IO ()) => IsString (a -> b) where
+    fromString a b = do
+        putStrLn a
+        putStrLn b
+
+main :: IO ()
+main = do
+    "hi" "hey"
