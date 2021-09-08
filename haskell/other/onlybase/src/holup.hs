b16cdaf8736252521891b303fb8eb4739e4eee9a
Dan Dart <git@dandart.co.uk>
2021-09-08 20:34:30 +0100

    Sneaky use of sequenceA

---
+++ haskell/other/onlybase/src/holup.hs
@@ -0,0 +1,11 @@
+main :: IO ()
+main = putStrLn holup
+
+holup :: String
+holup = sequenceA [
+    head,
+    last,
+    last . init,
+    foldr (.) id (replicate 9 succ) . last . init . init,
+    foldr (.) id (replicate 4 succ) . last . init
+    ] "Hello"
