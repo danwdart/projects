aa76fe938b495bd74506584eaab4d0024aebaf2a
Dan Dart <git@dandart.co.uk>
2021-10-09 01:48:48 +0100

    dci direct parsing to io

---
+++ haskell/other/onlybase/src/dci.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE UnicodeSyntax #-}
+{-# OPTIONS_GHC -Wno-orphans -Wno-unused-top-binds #-}
+
+import           GHC.Read
+import           Text.ParserCombinators.ReadP
+import           Text.ParserCombinators.ReadPrec (lift)
+
+commandParser :: ReadP (IO ())
+commandParser = do
+    cmd <- many get
+    pure $
+        putStrLn $ "Command was " ++ cmd
+
+instance Read (IO ()) where
+    readPrec = lift commandParser
+
+parseAndPerform :: IO ()
+parseAndPerform = do
+    cmd <- getLine
+    read cmd
+
+main :: IO ()
+main = parseAndPerform
