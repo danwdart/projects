180d890937d5f295e62e8571182428c2cd58ae6b
Dan Dart <git@dandart.co.uk>
2021-10-15 00:10:47 +0100

    semiprime

---
+++ haskell/maths/src/primefun/semiprime.hs
@@ -0,0 +1,13 @@
+{-# OPTIONS_GHC -Wno-unused-top-binds #-}
+
+import Data.Numbers.Primes
+
+main :: IO ()
+main = do
+    print [s `mod` (2 ^ n) | n <- [1..10]]
+    
+
+p1, p2, s :: Integer
+p1 = primes !! 10113
+p2 = primes !! 19736
+s = p1 * p2
