db3c98cc878f716a3de332cec58677cc7a75a794
Dan Dart <git@dandart.co.uk>
2021-11-08 01:37:50 +0000

    Make an undiscarded project

---
+++ haskell/undiscarded/lib/Control/Monad/State/Disparate.hs
@@ -0,0 +1,8 @@
+module Control.Monad.State.Disparate where
+
+import Control.Monad.Identity
+import Control.Monad.Trans.State.Disparate
+
+type StateDisparate s1 s2 = StateDisparateT s1 s2 Identity
+
+type StateDouble s1 s2 = StateDoubleT s1 s2 Identity
---
+++ haskell/undiscarded/lib/Control/Monad/Trans/State/Disparate.hs
@@ -0,0 +1,5 @@
+module Control.Monad.Trans.State.Disparate where
+
+newtype StateDisparateT s1 s2 m a = StateDisparateT (s1 -> m (a, s2))
+
+newtype StateDoubleT s1 s2 m a = StateDoubleT (s1 -> s2 -> m (a, (s1, s2)))
