973a31711c7630b615237c5de0f6c40b1cea6b98
Dan Dart <git@dandart.co.uk>
2021-11-28 22:47:18 +0000

    Jailbreak some deps

--- haskell/other/9.0.1/default.nix
+++ haskell/other/9.0.1/default.nix
@@ -1,10 +1,14 @@
 { nixpkgs ? import  (builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/master.tar.gz") {},
-  compiler ? "ghc901" }: # bmp requires old bytestring 
+  compiler ? "ghc921" }: # bmp requires old bytestring 
 let
   gitignore = nixpkgs.nix-gitignore.gitignoreSourcePure [ ./.gitignore ];
   lib = nixpkgs.pkgs.haskell.lib;
   myHaskellPackages = nixpkgs.pkgs.haskell.packages.${compiler}.override {
     overrides = self: super: rec {
+      bmp = lib.doJailbreak super.bmp;
+      carray = lib.doJailbreak super.carray;
+      flow = lib.doJailbreak super.flow;
+      OpenGLRaw = lib.doJailbreak super.OpenGLRaw;
       other901 = self.callCabal2nix "other901" (gitignore ./.) {};
     };
   };
@@ -16,16 +20,18 @@ let
       gen-hie > hie.yaml
       for i in $(find -type f); do krank $i; done
     '';
-    buildInputs = with nixpkgs; with haskellPackages; [
+    buildInputs = with myHaskellPackages; with nixpkgs; with haskellPackages; [
       apply-refact
       cabal-install
       ghcid
+      haskell-language-server
+      hasktags
       hlint
       implicit-hie
       krank
       stan
       stylish-haskell
-      weeder
+      haskellPackages.weeder # not on ghc 9.2 because of generic-lens-core issues
     ];
     withHoogle = false;
   };
