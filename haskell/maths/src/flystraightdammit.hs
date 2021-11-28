a5756da9d913fed2bf288059d8437229cbdd2da4
Dan Dart <git@dandart.co.uk>
2021-11-28 23:20:24 +0000

    FSD

--- haskell/maths/src/flystraightdammit.hs
+++ haskell/maths/src/flystraightdammit.hs
@@ -2,12 +2,24 @@
 
 module Main where
 
-import Data.List.Index
+divInt :: Integer -> Integer -> Integer
+divInt x y = round ((fromInteger x :: Double) / (fromInteger y :: Double))
 
-numbers :: [Integer]
-numbers = go [1,1] where
-    go :: [Integer] -> [Integer]
-    go xs = 1:go xs
+numbers :: [(Integer, Integer)]
+numbers = go (go [(1,1),(0,1)]) where
+    go :: [(Integer, Integer)] -> [(Integer, Integer)]
+    -- who cares
+    go [] = []
+    go [(_, _)] = []
+    -- here's the real deal
+    go all'@((i1,x1):_) = (
+        i1 + 1,
+        if gcd x1 (i1 + 1) == 1
+            then
+                (i1 + 1) + x1
+            else
+                i1 `divInt` gcd x1 (i1 + 1)
+        ):all'
 
 main :: IO ()
-main = pure ()
+main = print $ take 10 numbers
