9f1e474d26c8c537f3853334726a411d0f5d11be
Dan Dart <git@dandart.co.uk>
2021-12-03 21:47:12 +0000

    Weird calls - function monad

---
+++ haskell/other/onlybase/src/weird-calls.hs
@@ -0,0 +1,26 @@
+module Main where
+
+import Control.Monad
+
+main :: IO ()
+main = do
+    print $ dupMonad (+) 12
+    print $ bindFnInt succ (+) 2
+
+{-
+ $> :t join @((->) Int)
+ join @((->) Int)
+  :: Monad ((->) Int) => (Int -> (Int -> a)) -> Int -> a
+-}
+dupMonad :: (Int -> Int -> Int) -> Int -> Int
+dupMonad = join
+
+{-
+ $> :t (>>=) @((->) Int)
+ (>>=) @((->) Int) :: (Int -> a) -> (a -> Int -> b) -> Int -> b
+-}
+bindFn :: (a -> b) -> (b -> a -> c) -> a -> c
+bindFn = (>>=)
+
+bindFnInt :: (Int -> Int) -> (Int -> Int -> Int) -> Int -> Int
+bindFnInt = (>>=)
