fdbcafe5fb14f0052993884e7c01edf0b44dbd0f
Dan Dart <git@dandart.co.uk>
2021-12-05 23:03:31 +0000

    aoc2021 1_1

---
+++ haskell/aoc/2021/src/1_1.hs
@@ -0,0 +1,14 @@
+module Main where
+
+fnPairs :: Ord a => (a -> a -> b) -> [a] -> [b]
+fnPairs _ [] = []
+fnPairs _ [_] = []
+fnPairs fn (x1:x2:xs) = fn x1 x2:fnPairs fn (x2:xs)
+
+main :: IO ()
+main = do
+    string <- readFile "data/1"
+    let strings = lines string
+    let numbers = fmap read strings :: [Integer]
+    let soln = length . filter (== True) $ fnPairs (<) numbers
+    print soln
