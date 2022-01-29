d3f124753a2f598f8e391ab713fcba8c1cfa589a
Dan Dart <git@dandart.co.uk>
2022-01-29 02:23:35 +0000

    A025281

---
+++ haskell/maths/src/oeis/A025281.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE PostfixOperators #-}
+
+import Data.List
+import Data.Numbers.Primes
+
+(!) :: Integer -> Integer
+(!) 0 = 1
+(!) 1 = 1
+(!) n = n * ((n-1) !)
+
+values :: [Integer]
+values = sum .
+    (\n -> 
+        fmap (
+            (\(x, r) -> x * fromIntegral r) .
+            (\xs -> (head xs, length xs))
+        ) .
+        group $ primeFactors (n !)
+    ) <$> [1..20]
+
+
+main :: IO ()
+main = print values
