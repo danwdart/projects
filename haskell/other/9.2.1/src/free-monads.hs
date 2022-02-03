4f4c9a0782ae536910480dbb265aca8d050d7e95
Dan Dart <git@dandart.co.uk>
2022-02-03 20:23:12 +0000

    Even more examples of free monads

--- haskell/other/9.2.1/src/free-monads.hs
+++ haskell/other/9.2.1/src/free-monads.hs
@@ -5,32 +5,68 @@
 -- https://serokell.io/blog/introduction-to-free-monads
 import Control.Monad.Free
 
-data ProgramF t a
+data IOF t a
   = Input (t -> a)
   | Output t a
+  | ReadFile t (t -> a)
+  | WriteFile t t a
   deriving Functor
 
-program :: Free (ProgramF String) ()
+type FreeIOF t = Free (IOF t)
+
+readStrLnF :: FreeIOF String String
+readStrLnF = liftF $ Input id
+
+putStrLnF :: String -> FreeIOF String ()
+putStrLnF str = liftF $ Output str ()
+
+readFileF :: String -> FreeIOF String String
+readFileF filename = liftF $ ReadFile filename id
+
+writeFileF :: String -> String -> FreeIOF String ()
+writeFileF filename contents = liftF $ WriteFile filename contents ()
+
+program :: FreeIOF String ()
 program = do
-  x <- liftF $ Input id
-  liftF $ Output ("hi " <> x) ()
+    putStrLnF "Please enter file to read."
+    fileToRead <- readStrLnF
+    putStrLnF "Please enter file to write."
+    fileToWrite <- readStrLnF
+    putStrLnF $ "Going to read from " <> fileToRead
+    contents <- readFileF fileToRead
+    putStrLnF contents
+    putStrLnF $ "Going to write to " <> fileToWrite
+    writeFileF fileToWrite contents
+    putStrLnF "Done!"
 
-interpret :: Free (ProgramF String) () -> IO ()
+interpret :: FreeIOF String () -> IO ()
 interpret = foldFree $ \case
-    Input f -> f <$> getLine
-    Output x f -> do
-        putStrLn x
-        pure f
+    Input next -> next <$> getLine
+    Output str next -> next <$ putStrLn str
+    ReadFile filename next -> next <$> readFile filename
+    WriteFile filename contents next -> next <$ writeFile filename contents
 
-pretty :: Free (ProgramF String) () -> String
-pretty (Pure _) = ""
-pretty (Free f) = case f of
-    Input next -> "Input (demo)\n" <> pretty (next "demo")
-    Output x next -> "Output: " <> x <> pretty next
+mockFiles :: FreeIOF String () -> IO ()
+mockFiles = foldFree $ \case
+    Input next -> next <$> getLine
+    Output str next -> next <$ putStrLn str
+    ReadFile filename next -> pure $ next filename
+    WriteFile _ _ next -> pure next
+
+pretty :: String -> FreeIOF String () -> String
+pretty _ (Pure _) = ""
+pretty demoInput (Free f) = case f of
+    Input next -> "Input (" <> demoInput <> ")\n" <> pretty demoInput (next demoInput)
+    Output str next -> "Output: " <> str <> "\n" <> pretty demoInput next
+    ReadFile filename next -> "Read file: " <> filename <> "\n" <> pretty demoInput (next demoInput) 
+    WriteFile filename contents next -> "Write file: " <> filename <> " with contents of length " <> show (length contents) <> "\n" <> pretty demoInput next
 
 main :: IO ()
 main = do
     putStrLn "Running this:"
-    putStrLn $ pretty program
-    putStrLn "Here we go:"
+    putStrLn $ pretty "Demo" program
+    putStrLn "Fake version:"
+    mockFiles program
+    putStrLn "Real version:"
     interpret program
+    
