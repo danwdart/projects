e4bd6ed5d0268c66fae0a6ffd3ae7227dcd51343
Dan Dart <git@dandart.co.uk>
2022-02-10 17:41:22 +0000

    effects

---
+++ haskell/other/9.2.1/src/effects.hs
@@ -0,0 +1,20 @@
+{-# LANGUAGE DataKinds #-}
+{-# OPTIONS_GHC -Wno-unused-matches  #-}
+
+module Main where
+
+import Control.Monad.Freer
+import Control.Monad.Freer.State
+import Control.Monad.Freer.Writer
+
+type MyM = Eff '[State Int, Writer [String]]
+
+eff :: MyM String
+eff = do
+    tell ["hi"]
+    put (3 :: Int)
+    o <- get :: MyM Int
+    pure . show $ o
+
+main :: IO ()
+main = print . run . runWriter . evalState 2 $ eff
