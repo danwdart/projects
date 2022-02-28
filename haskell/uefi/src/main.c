68ef23f9d2d153555d620caaedd1f83c4a1da1a5
Dan Dart <git@dandart.co.uk>
2022-02-28 18:35:36 +0000

    uefi

--- haskell/uefi/src/main.c
+++ haskell/uefi/src/main.c
@@ -1,8 +1,8 @@
 #include <uefi.h>
 #include "hsmain_stub.h"
 
-int main(void) {
-    hs_init(void 0, void 0);
+int main(int argc, char *argv[]) {
+    hs_init(&argc, &argv);
     /* hs_run(); */
     kmain();
     return 0;
