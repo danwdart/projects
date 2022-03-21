6a8860a621d169ff1b73b18792f83590213633f7
Dan Dart <git@dandart.co.uk>
2022-03-21 05:04:35 +0000

    aoc2021: upgrades

--- haskell/aoc/2021/default.nix
+++ haskell/aoc/2021/default.nix
@@ -1,8 +1,11 @@
-{ nixpkgs ? import  (builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/master.tar.gz") {},
-  compiler ? "ghc921" }:
+{ nixpkgs ? import (builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/haskell-updates.tar.gz") {},
+  haskell-tools ? import (builtins.fetchTarball "https://github.com/danwdart/haskell-tools/archive/master.tar.gz") {},
+  compiler ? "ghc922"
+}:
 let
   gitignore = nixpkgs.nix-gitignore.gitignoreSourcePure [ ./.gitignore ];
   lib = nixpkgs.pkgs.haskell.lib;
+  tools = haskell-tools compiler;
   myHaskellPackages = nixpkgs.pkgs.haskell.packages.${compiler}.override {
     overrides = self: super: rec {
       aoc2021 = self.callCabal2nix "aoc2021" (gitignore ./.) {};
@@ -18,19 +21,7 @@ let
     packages = p: [
       p.aoc2021
     ];
-    buildInputs = with myHaskellPackages; with nixpkgs; with haskellPackages; [
-      apply-refact
-      cabal-install
-      ghcid
-      haskell-language-server
-      hasktags
-      hlint
-      implicit-hie
-      krank
-      stan
-      stylish-haskell
-      haskellPackages.weeder # not on ghc 9.2 because of generic-lens-core issues
-    ];
+    buildInputs = tools.defaultBuildTools;
     # withHoogle = false;
   };
   exe = lib.justStaticExecutables (myHaskellPackages.aoc2021);
