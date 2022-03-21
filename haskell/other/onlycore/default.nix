2ad143ca451ab96a27b0e21d37398742ae1e953e
Dan Dart <git@dandart.co.uk>
2022-03-21 04:22:53 +0000

    onlycore: upgrades

--- haskell/other/onlycore/default.nix
+++ haskell/other/onlycore/default.nix
@@ -1,7 +1,11 @@
-{ nixpkgs ? import  (builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/master.tar.gz") {},
-  compiler ? "ghc921" }:
+{
+  nixpkgs ? import (builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/haskell-updates.tar.gz") {},
+  haskell-tools ? import (builtins.fetchTarball "https://github.com/danwdart/haskell-tools/archive/master.tar.gz") {},
+  compiler ? "ghc922"
+}:
 let
   gitignore = nixpkgs.nix-gitignore.gitignoreSourcePure [ ./.gitignore ];
+  tools = haskell-tools compiler;
   lib = nixpkgs.pkgs.haskell.lib;
   myHaskellPackages = nixpkgs.pkgs.haskell.packages.${compiler}.override {
     overrides = self: super: rec {
@@ -16,19 +20,7 @@ let
       gen-hie > hie.yaml
       for i in $(find -type f); do krank $i; done
     '';
-    buildInputs = with myHaskellPackages; with nixpkgs; with haskellPackages; [
-      apply-refact
-      cabal-install
-      ghcid
-      haskell-language-server
-      hasktags
-      hlint
-      implicit-hie
-      krank
-      stan
-      stylish-haskell
-      haskellPackages.weeder # not on ghc 9.2 because of generic-lens-core issues
-    ];
+    buildInputs = tools.defaultBuildTools;
     withHoogle = false;
   };
   exe = lib.justStaticExecutables (myHaskellPackages.onlycore);
@@ -36,6 +28,7 @@ in
 {
   inherit shell;
   inherit exe;
-  inherit myHaskellPackages;
+  
+   inherit myHaskellPackages;
   onlycore = myHaskellPackages.onlycore;
 }
