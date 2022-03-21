513bbde665aa42a8be92e034f1de176b24e7d793
Dan Dart <git@dandart.co.uk>
2022-03-21 05:07:45 +0000

    upgrade ghc in haskell

--- haskell/shell.nix
+++ haskell/shell.nix
@@ -1,7 +1,7 @@
-with import (builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/master.tar.gz") {};
+with import (builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/haskell-updates.tar.gz") {};
 runCommand "haskell" {
     buildInputs = [
-        haskell.compiler.ghc921
-        cabal-install
+        haskell.compiler.ghc922
+        haskell.packages.ghc922.cabal-install
     ];
 } ""
