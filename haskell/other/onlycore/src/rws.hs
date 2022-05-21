9a3bac345f45ab49bc7631a03ad6eca6d4d2fed5
Dan Dart <git@dandart.co.uk>
2022-05-21 23:43:46 +0000

    move in rws

---
+++ haskell/other/onlycore/src/rws.hs
@@ -0,0 +1,28 @@
+{-# LANGUAGE PackageImports        #-}
+
+import           Control.Monad.IO.Class
+import           "mtl" Control.Monad.RWS
+
+type AppRead = String
+type AppState = Int
+type AppWriter = [String]
+type AppMonad = IO
+type AppReturn = Char
+
+stuff ∷ RWS AppRead AppWriter AppState AppReturn
+stuff = do
+    tell ["hi"]
+    put 2
+    asks (!! 0)
+
+stuff2 ∷ RWST AppRead AppWriter AppState AppMonad AppReturn
+stuff2 = do
+    tell ["hey"]
+    liftIO . print $ "AAA"
+    put 2
+    asks (!! 0)
+
+main ∷ IO ()
+main = do
+    print $ runRWS stuff "aeiou" 1
+    print =<< runRWST stuff2 "aeiou" 1
