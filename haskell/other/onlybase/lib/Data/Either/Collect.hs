8a34397ed9a5c28985d1dfa49406bfbfa63cc674
Dan Dart <git@dandart.co.uk>
2022-06-09 19:50:57 +0100

    collectEithers

---
+++ haskell/other/onlybase/lib/Data/Either/Collect.hs
@@ -0,0 +1,11 @@
+module Data.Either.Collect where
+
+import Data.Either
+
+-- | Collects either all Lefts if there are any, otherwise return all Rights.
+-- >>> collectEithers [Left 1, Right 2, Left 3, Right 4]
+collectEithers :: [Either e a] -> Either [e] [a]
+collectEithers xs = if null . fst $ p
+    then Right $ snd p
+    else Left $ fst p
+        where p = partitionEithers xs
