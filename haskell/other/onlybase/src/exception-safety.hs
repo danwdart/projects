5bb0ef46b5ec8b78eb41fcadbba193a594089bb3
Dan Dart <git@dandart.co.uk>
2022-06-09 18:27:06 +0100

    exception safety

--- haskell/other/onlybase/src/exception-safety.hs
+++ haskell/other/onlybase/src/exception-safety.hs
@@ -1,18 +1,42 @@
+{-# LANGUAGE DeriveAnyClass #-}
+
 import           Control.Exception
 import           Control.Monad.Fail
+import           GHC.Stack
+
 -- import System.IO
 -- now there's a try and an exception monad
 
-type IOStrEx = IO (Either SomeException String)
+-- This is just for IO exceptions and not for MonadError, that'll be somewhere else
 
-tryRead ∷ String → IOStrEx
-tryRead = try.readFile
+tryRead ∷ HasCallStack => String → IO (Either SomeException String)
+tryRead = try . readFile
+
+sampleIO :: HasCallStack => IO ()
+sampleIO = throw $ userError "Bob"
+
+failer :: HasCallStack => IO String
+failer = fail "Bob"
+
+tryer :: IO (Either SomeException String)
+tryer = try failer
+
+data DanException = DanException deriving (Show, Exception)
+
+sampleHandler :: SomeException -> IO ()
+sampleHandler (SomeException ex) = putStrLn $ "Caught error: (" <> displayException ex <> ")"
+
+danHandler :: DanException -> IO ()
+danHandler d@DanException = putStrLn $ "Caught DanException: " <> displayException d
 
 main ∷ IO ()
 main = do
     tryRead "bob" >>= print
     writeFile "Jim" "Contents"
     tryRead "Jim" >>= print
-    _ <- pure (try $ Control.Monad.Fail.fail "Bob" :: IOStrEx)
-    pure ()
-    -- userError "Bob"
+    tryer >>= print
+    sampleIO `catch` danHandler `catch` sampleHandler
+    throw DanException `catch` danHandler `catch` sampleHandler
+    throw (userError "Bob again") `catch` danHandler `catch` sampleHandler
+    catch @DanException (throw DanException) danHandler
+    catch @SomeException (throw $ userError "Bob again again") sampleHandler
