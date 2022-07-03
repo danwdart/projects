d2a4a481216c9120f16d07d2946a7e5cec4f041e
Dan Dart <git@dandart.co.uk>
2022-07-03 15:15:58 +0100

    A355467

---
+++ haskell/maths/src/oeis/A355467.hs
@@ -0,0 +1,14 @@
+import Data.Numbers.Primes
+
+main :: IO ()
+main = print $ take 200 result
+
+result :: [Integer]
+result = fmap (
+    \n -> head (
+        dropWhile (
+            \m -> length (primeFactors m :: [Integer]) <= length (primeFactors n :: [Integer])
+        )
+        [n..]
+    )
+    ) [1..]
