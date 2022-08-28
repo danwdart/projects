692413273423bbab484787784714aca951965bee
Dan Dart <git@dandart.co.uk>
2022-08-28 14:27:22 +0100

    update std to ghc942 but no cabal-install

--- haskell/shell.nix
+++ haskell/shell.nix
@@ -1,7 +1,7 @@
 with import (builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/haskell-updates.tar.gz") {};
 runCommand "haskell" {
     buildInputs = [
-        haskell.compiler.ghc941
-        haskell.packages.ghc941.cabal-install
+        haskell.compiler.ghc942
+        haskell.packages.ghc942.cabal-install
     ];
 } ""
