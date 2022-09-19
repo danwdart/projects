e9d48462191790af324829ca8a3dbed99a4cf8a2
Dan Dart <git@dandart.co.uk>
2022-09-20 00:51:01 +0100

    errors to be refactored

---
+++ haskell/other/onlybase/src/errors.hs
@@ -0,0 +1,76 @@
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+
+import Control.Exception
+
+newtype DBConnectionInfo = DBConnectionInfo String
+    deriving Show
+
+newtype RowId = RowId Int
+    deriving Show
+    -- deriving (Num) -- only need fromInteger so nm
+
+data DBRow = DBRow {
+    dbRowId :: RowId,
+    dbRowName :: String
+} deriving (Show)
+
+sampleRow :: DBRow
+sampleRow = DBRow (RowId 1) "Datum"
+
+newtype Url = Url String
+    deriving Show
+
+dbConn :: DBConnectionInfo
+dbConn = DBConnectionInfo "DB Conn Info"
+
+rowId :: RowId
+rowId = RowId 123
+
+data NonsenseException = NonsenseException String | UnexpectedException
+    deriving (Show, Exception)
+
+formatRow :: DBRow -> Either NonsenseException String
+formatRow DBRow { dbRowId = RowId rowId, dbRowName = dbRowName' }
+    | rowId == 0 = Left $ NonsenseException dbRowName'
+    | rowId == 3 = Left $ UnexpectedException
+    | otherwise = Right $ "The row ID was " <> show rowId <> " and the row name was " <> dbRowName'
+
+data DBException = RowNotFoundException RowId | DBConnectionException DBConnectionInfo
+    deriving (Show, Exception)
+
+getRow :: Int -> Either DBException DBRow
+getRow id
+    | id == 2 = Left $ RowNotFoundException (RowId 2)
+    | id == 0 = Left $ DBConnectionException dbConn
+    | otherwise = Right sampleRow { dbRowId = RowId id }
+
+data ClientException = NotFoundException Url | InternalServerError
+    deriving (Show, Exception)
+
+-- TODO refactor first/second
+fetchRow :: Url -> Int -> Either ClientException String
+fetchRow url id = case getRow id of
+    Left ex -> case ex of
+        RowNotFoundException rowId -> Left $ NotFoundException url
+        DBConnectionException dbConnInfo -> Left InternalServerError -- todo logger
+    Right row -> case formatRow row of
+        Left ex2 -> case ex2 of
+            NonsenseException dbRowName -> Left InternalServerError
+            UnexpectedException -> Left $ NotFoundException (Url "place")
+        Right formatted -> Right formatted
+
+main :: IO ()
+main = do
+    let xs = fetchRow (Url "url") <$> [0..5]
+
+    -- todo MonadError
+    -- todo indexed
+    mapM_ (\case
+        Left x -> putStrLn $ "Error: " <> show x
+        Right res' -> putStrLn $ "Found: " <> show res'
+        ) xs
+    -- todo throwIO?
