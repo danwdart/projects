c0ae721c85f0a329dba1aec0d38bb2e571109d74
Dan Dart <git@dandart.co.uk>
2022-09-19 02:08:05 +0100

    windows MessageBox example (x86_32)

---
+++ c/cross/windows/x86_32/src/windows.c
@@ -0,0 +1,13 @@
+#include <windows.h>
+#include <winuser.h>
+
+int main(int argc, char *argv[]) {
+    MessageBox(
+        NULL,
+        "Goteem",
+        "o///",
+        MB_ICONINFORMATION | MB_OK | MB_DEFBUTTON2
+    );
+
+    return 0;
+}
