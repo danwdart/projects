ba1d99de7fe6158703c4c212f931a191590e0004
Dan Dart <git@dandart.co.uk>
2022-09-24 02:43:48 +0100

    summarise ohsol game

--- haskell/cards/src/ohsol.hs
+++ haskell/cards/src/ohsol.hs
@@ -1,4 +1,4 @@
-{-# OPTIONS_GHC -Wno-unused-top-binds -Wno-unused-matches -Wno-incomplete-patterns #-}
+{-# OPTIONS_GHC -Wno-unused-top-binds -Wno-unused-matches -Wno-incomplete-patterns -Wno-unused-imports #-}
 
 -- https://en.wikipedia.org/wiki/One-handed_solitaire
 
@@ -8,12 +8,13 @@ import           Data.Function
 import           Deck
 import           Suit.Unbounded
 import           System.Random.Shuffle
+import           Text.Printf
 import           Value.Unbounded
 
 main ∷ IO ()
 main = do
     game <- playUntilEnd <$> initialGameState
-    print game
+    putStrLn . summariseGame $ game
 
 type Current = DeckUnbounded
 type InPlay = DeckUnbounded
@@ -21,6 +22,17 @@ type Discard = DeckUnbounded
 type Game = (Current, InPlay, Discard) -- todo data / iter
 data GameState = InProgress | Won | Lost deriving (Eq, Show)
 data GameMove = TakeOne | DiscardTwo | DiscardFour | End deriving (Eq, Show)
+
+summariseGame :: Game -> String
+summariseGame game@(curr, inp, disc) = "Game: " <>
+    show (gameState game) <>
+    ", with " <>
+    show (length curr) <>
+    " cards left, " <>
+    show (length inp) <>
+    " cards in play (available) and " <>
+    show (length disc) <>
+    " cards discarded."
 
 fullPack ∷ DeckUnbounded
 fullPack = Deck $ Card <$> fmap Value [1..13] <*> fmap Suit [1..4]
@@ -46,14 +58,11 @@ gameState (Deck c, Deck ip, _)
 nextGameMove ∷ Game → GameMove
 nextGameMove (Deck [], Deck [], _) = End
 nextGameMove (_, Deck [], _) = End
-nextGameMove (Deck [], _, _) = TakeOne
-nextGameMove (Deck [c1], _, _) = TakeOne
 nextGameMove (Deck (c1:_:_:c4:_), _, _)
     | sameSuit c1 c4 = DiscardTwo
     | sameValue c1 c4 = DiscardFour
     | otherwise = TakeOne
-nextGameMove (Deck (c1:c2:c3:_), _, _) = TakeOne
-nextGameMove (Deck (c1:c2:_), _, _) = TakeOne
+nextGameMove _ = TakeOne
 
 takeOne ∷ Game → Game
 takeOne (Deck cs, Deck (ip:ips), ds) = (Deck (ip:cs), Deck ips, ds)
