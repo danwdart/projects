8a5c5ace46b7f96cb1e78637d145ffc654fab992
Dan Dart <git@dandart.co.uk>
2022-10-13 22:22:56 +0100

    some maths

--- haskell/maths/src/harmonic-limit.hs
+++ haskell/maths/src/harmonic-limit.hs
@@ -8,6 +8,10 @@ import           Data.Tuple.Extra
 
 -- *>>
 
+-- A004080 		Least k such that H(k) >= n, where H(k) is the harmonic number Sum_{i=1..k} 1/i. 
+
+-- e.g. 1, 4, 11, 31, 83, 227, 616, 1674
+-- meaning: H(1) is just over 1, H(4) is just over 2, H(11) is just over 3, H(31) is just over 4, etc.
 result ∷ [(Int, Int)]
 result = nubOrdOn snd . fmap (both floor) $ scanl1 (\(_,b0) (a1, b1) -> (a1, b0 + b1)) [ (x,1/x) | x <- [1..1000000000000000]]
 
