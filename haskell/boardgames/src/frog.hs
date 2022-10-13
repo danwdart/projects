3d07920fbfadd0d65a85c68a4fb814a475fec1ed
Dan Dart <git@dandart.co.uk>
2022-10-13 22:23:25 +0100

    update frog

--- haskell/boardgames/src/frog.hs
+++ haskell/boardgames/src/frog.hs
@@ -1,11 +1,26 @@
 {-# OPTIONS_GHC -Wno-unused-top-binds #-}
 
+-- Expected number of jumps
+-- Simulation
+-- Formula
+
 import           Control.Monad
-import           System.Random
+import           Control.Monad.Random
+import           Data.Foldable as Foldable
+import           Data.Map (Map)
+import qualified Data.Map as M
+-- import Data.Ratio
+-- import Debug.Trace
+-- import           System.Random
 
 main ∷ IO ()
-main = print . avg =<< replicateM 1000000 (randomJump 1 10)
+main = do
+    putStrLn "Running the simulation. This may take a few seconds..."
+    simResults <- replicateM 1000000 (randomJump 1 10)
+    print . avg $ simResults
+    print . freqList $ simResults
 
+-- sim
 randomJump ∷ Int → Int → IO Int
 randomJump js total = do
     a <- randomRIO (1, total) :: IO Int
@@ -13,8 +28,25 @@ randomJump js total = do
     then randomJump (js + 1) (total - a)
     else pure js
 
-freqList ∷ [Int] → [(Int, Int)]
-freqList = undefined
+freqList ∷ [Int] → Map Int Int
+freqList = Foldable.foldr' (\cur -> M.insertWith (const succ) cur 1) M.empty
 
 avg ∷ [Int] → Double
 avg xs = fromIntegral (sum xs) / fromIntegral (length xs)
+
+-- calc
+harmonic :: Integer -> Rational
+harmonic x = sum $ recip . fromIntegral <$> [1..x]
+
+-- Answer should be:
+
+-- PJ1: 1 % 10
+-- PJ2: 1 % 10 * H(9)
+-- PJ3: 1 % 10 * 
+-- PJ4:
+-- PJ5:
+-- PJ6:
+-- PJ7:
+-- PJ8: 
+-- PJ9: 
+-- PJ10: H(10)
