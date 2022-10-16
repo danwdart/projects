e5de7dd267869a0627c0bcaa0ca173dccd9925c5
Dan Dart <git@dandart.co.uk>
2022-10-16 02:50:02 +0100

    update bench

---
+++ haskell/other/9.4.2/bench/recursiveFacFibs.hs
@@ -0,0 +1,65 @@
+{-# LANGUAGE BangPatterns #-}
+
+module Main where
+
+import Criterion.Main
+
+import Data.Dynamic
+import Data.Foldable
+import Data.Maybe
+import qualified Data.Array as A
+import qualified Data.Vector as V
+
+facsNaive :: [Integer]
+facsNaive = undefined
+
+facNaive :: Integer -> Integer
+facNaive 0 = 1
+facNaive 1 = 1
+facNaive x = x * facNaive (x - 1)
+
+facsTailRecursive :: [Integer]
+facsTailRecursive = undefined
+
+facTailRecursive :: Integer -> Integer
+facTailRecursive x = go x 1 where
+    go :: Integer -> Integer -> Integer
+    go 1 acc = acc
+    go x acc = go (x - 1) (x * acc)
+
+fibsNaive :: [Integer]
+fibsNaive = undefined
+
+fibNaive :: Integer -> Integer
+fibNaive 0 = 0
+fibNaive 1 = 1
+fibNaive x = fibNaive (x - 1) + fibNaive (x - 2)
+
+fibsTailRecursive :: [Integer]
+fibsTailRecursive = undefined
+
+fibTailRecursive :: Integer -> Integer
+fibTailRecursive = undefined -- todo tuple swapping and stuff
+
+main = defaultMain [
+    bgroup "fac" [
+        {-bgroup "list" [
+            bench "naive" $ nf (facsNaive !!) 100,
+            bench "tail recursive" $ nf (facsTailRecursive !!) 100
+            ],-}
+        bgroup "direct" [
+            bench "naive" $ nf facNaive 100,
+            bench "tail recursive" $ nf facTailRecursive 100
+            ]
+        ]{-,
+    bgroup "fib" [
+        bgroup "list" [
+            bench "naive" $ nf (fibsNaive !!) 100,
+            bench "tail recuesive" $ nf (fibsTailRecursive !!) 100
+            ],
+        bgroup "direct" [
+            bench "naive" $ nf fibsNaive 100,
+            bench "tail recuesive" $ nf fibsTailRecursive 100
+            ]
+        ]-}
+    ]
---
+++ haskell/other/9.4.2/bench/sums.hs
@@ -0,0 +1,39 @@
+{-# LANGUAGE BangPatterns #-}
+
+module Main where
+
+import Criterion.Main
+
+import Data.Dynamic
+import Data.Foldable
+import Data.Maybe
+import qualified Data.Array as A
+import qualified Data.Vector as V
+
+elements :: Int
+elements = 1000000
+
+-- TODO ensure that this gets fully evaluated before timing
+listRepl :: a -> [a]
+listRepl = replicate elements
+
+list :: [Int]
+list = listRepl (1 :: Int)
+
+vecRepl :: a -> V.Vector a
+vecRepl = V.replicate elements
+
+arrRepl :: a -> A.Array Int Int
+arrRepl n = A.listArray (0, elements - 1) [0..elements - 1]
+
+
+main = defaultMain [
+    bgroup "sums" [
+        bench "get list" $ nf list,
+        bench "sum" $ nf sum list,
+        bench "foldl" $ nf (foldl (+) 0) list,
+        bench "foldl'" $ nf (foldl' (+) 0) list,
+        bench "foldr" $ nf (foldr (+) 0) list,
+        bench "foldr'" $ nf (foldr' (+) 0) list
+        ]
+    ]
