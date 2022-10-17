4fb993b0520b4561388d67811e4b710ab4850b2c
Dan Dart <git@dandart.co.uk>
2022-10-17 17:38:54 +0100

    mmio base

---
+++ haskell/other/9.4.2/src/mmio.hs
@@ -0,0 +1,11 @@
+{-# LANGUAGE Trustworthy #-}
+{-# OPTIONS_GHC -Wno-unsafe -Wno-unused-imports #-}
+
+import System.IO.MMap
+import Foreign.Ptr
+
+-- direct foreign ptr
+-- mmio files
+
+main :: IO ()
+main = pure ()
