8d0a3d16184de0ce37ca791225ae6e13e76b9571
Dan Dart <git@dandart.co.uk>
2022-11-02 19:30:16 +0000

    fix build of validation

--- haskell/other/9.4.2/src/validation.hs
+++ haskell/other/9.4.2/src/validation.hs
@@ -2,15 +2,27 @@ module Main where
 
 import Data.Either.Validation
 
+ke :: Either String String
+ke = Left "Why?: "
+
+je :: Either String String
+je = Left "This"
+
+me :: Either String String
+me = Right "Yeaah."
+
+k :: Validation String String
+k = Failure "Why?: "
+
+j :: Validation String String
+j = Failure "This"
+
+m :: Validation String String
+m = Success "Yeaah."
+
 main :: IO ()
 main = do
-    let ke = Left "Why?: "
-    let je = Left "This"
-    let me = Right "Yeaah."
-    print @(Either String String) (ke <> je)
-    print @(Either String String) me
-    let k = Failure "Why?: "
-    let j = Failure "This"
-    let m = Success "Yeaah."
-    print @(Validation String String) (k <> j)
-    print @(Validation String String) m
+    print $ ke <> je
+    print me
+    print $ k <> j
+    print m
