28f1b1e7fc909118c5ef249bb233b2e4d5eb6a8b
Dan Dart <git@dandart.co.uk>
2023-01-03 18:16:00 +0000

    fix tumblr-editor build

--- haskell/tumblr-editor/default.nix
+++ haskell/tumblr-editor/default.nix
@@ -14,10 +14,6 @@ let
     overrides = self: super: rec {
       tumblr-editor = lib.dontHaddock (self.callCabal2nix "tumblr-editor" (gitignore ./.) {});
       colourista = lib.doJailbreak super.colourista;
-      webdriver = self.callCabal2nix "webdriver" (builtins.fetchGit {
-        url = "https://github.com/danwdart/hs-webdriver.git";
-        rev = "52a82be322cbb8ee8e65f87056827a3b89277e2a";
-      }) {};
     };
   };
   shell = myHaskellPackages.shellFor {
