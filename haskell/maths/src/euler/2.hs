6a373d247b38b4d07c35360afbfe0384671fe093
Dan Dart <git@dandart.co.uk>
2023-01-25 21:44:04 +0000

    remove project euler from maths

--- haskell/maths/src/euler/2.hs
+++
@@ -1,8 +0,0 @@
-result ∷ Int
-result = sum . filter even $ last (takeWhile (all (< 4000000)) $ iterate sumIter [2,1])
-    where
-        sumIter (a:b:xs) = (a + b):(a:b:xs)
-        sumIter _        = []
-
-main ∷ IO ()
-main = print result
