5eda6e2cb7a86f636667c296de148cd30b26b827
Dan Dart <git@dandart.co.uk>
2023-03-06 12:28:57 +0000

    Common ancestors

---
+++ haskell/maths/src/common-ancestors.hs
@@ -0,0 +1,20 @@
+module Main where
+
+-- Start with a number of people.
+-- Get another of the same number of people "above".
+-- For everyone, determine two of the same number of people from the above set, and set the above set to which indices of the below set exists.
+-- Get another set above
+-- Do the same, and merge the set numbers.
+-- Do until one of the set's descendants contains every or no descendents.
+
+{-}
+import Data.Set
+
+type Row = [Set Int]
+
+type Tower = [Row]
+
+-}
+
+main :: IO ()
+main = pure ()
