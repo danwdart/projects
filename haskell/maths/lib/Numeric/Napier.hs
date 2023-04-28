c7fde1210f1d25c8a978fe2f1f3d2a6f98cca405
Dan Dart <git@dandart.co.uk>
2023-04-29 00:36:59 +0100

    libraryify napier and add  demo example for it

---
+++ haskell/maths/lib/Numeric/Napier.hs
@@ -0,0 +1,29 @@
+module Numeric.Napier where
+
+import           Data.Digits   (digitsRev)
+import           Data.Function ((&))
+import           Data.Functor  ((<&>))
+import           Data.List     (elemIndex)
+import           Data.Maybe    (catMaybes)
+
+napierSymbols ∷ String
+napierSymbols = ['a'..'z'] <> ['A'..'Z']
+
+newtype Locar = Locar { getNumber :: Int }
+
+toMaybe ∷ a → Bool → Maybe a
+toMaybe l b =
+    if b
+    then Just l
+    else Nothing
+
+-- todo filter by list instead of that monstrosity
+
+instance Show Locar where
+    show (Locar n) = digitsRev 2 n <&> (1 ==) & zipWith toMaybe napierSymbols & catMaybes & reverse
+
+fromString ∷ String → Locar
+fromString a = a & filter (`elem` napierSymbols) <&> (`elemIndex` napierSymbols) & catMaybes <&> (2 ^) & sum & Locar
+
+symToInt ∷ String → Int
+symToInt = getNumber . fromString
--- haskell/maths/src/napier-locar.hs
+++ haskell/maths/src/napier-locar.hs
@@ -1,32 +1,22 @@
-{-# OPTIONS_GHC -Wno-unused-top-binds #-}
+import Numeric.Napier
 
-import           Data.Digits   (digitsRev)
-import           Data.Function ((&))
-import           Data.Functor  ((<&>))
-import           Data.List     (elemIndex)
-import           Data.Maybe    (catMaybes)
-
-napierSymbols ∷ String
-napierSymbols = ['a'..'z'] <> ['A'..'Z']
-
-newtype Locar = Locar { getNumber :: Int }
-
-toMaybe ∷ a → Bool → Maybe a
-toMaybe l b =
-    if b
-    then Just l
-    else Nothing
-
--- todo filter by list instead of that monstrosity
-
-instance Show Locar where
-    show (Locar n) = digitsRev 2 n <&> (1 ==) & zipWith toMaybe napierSymbols & catMaybes & reverse
-
-fromString ∷ String → Locar
-fromString a = a & filter (`elem` napierSymbols) <&> (`elemIndex` napierSymbols) & catMaybes <&> (2 ^) & sum & Locar
-
-symToInt ∷ String → Int
-symToInt = getNumber . fromString
+-- Words which are in alphabetical order (either) and with no repeats.
+alphabeticalWords :: [String]
+alphabeticalWords = [
+    "abhors",
+    "almost",
+    "begin",
+    "begins",
+    "biopsy",
+    "chimp",
+    "chimps",
+    "chintz",
+    "sponge",
+    "sponged",
+    "wronged"
+    ]
 
 main ∷ IO ()
-main = pure ()
+main = mapM_ (\word ->
+    putStrLn $ "The word " <> word <> " translated to Napier notation is equal to the value " <> show (symToInt word)
+    ) alphabeticalWords
