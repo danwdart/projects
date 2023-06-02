90aa6057bf5d0d49f88112c153d0aeb5e3dd8a27
Dan Dart <git@dandart.co.uk>
2023-06-02 22:28:41 +0100

    draft nonary

---
+++ haskell/maths/src/nonary.hs
@@ -0,0 +1,22 @@
+{-# LANGUAGE DataKinds #-}
+
+module Main where
+
+import Data.Kind
+import GHC.TypeNats
+
+-- data Nonary = Zero | One | Two | Three | Four | Five | Six | Seven | Eight
+
+type InBase :: Nat -> Type -> Type
+data InBase n a = InBase a
+
+instance Num (InBase n a) where
+    InBase x + InBase y = mod (natVal _) $ x + y
+    InBase x * InBase y = mod (natVal _) $ x + y
+    abs (InBase x) = _
+    signum (InBase x) = _
+    fromInteger x = InBase (mod _ x)
+    negate x = InBase _
+
+main :: IO ()
+main = pure ()
