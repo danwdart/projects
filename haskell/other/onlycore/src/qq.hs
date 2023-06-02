d3e1dcbcd005f4bb0baef28f895f30eef67a0edd
Dan Dart <git@dandart.co.uk>
2023-06-02 22:29:31 +0100

    qq

---
+++ haskell/other/onlycore/src/qq.hs
@@ -0,0 +1,17 @@
+{-# LANGUAGE QuasiQuotes #-}
+
+module Main where
+
+import TH
+
+someone :: String
+someone = "You"
+
+-- These don't get replaced by default, it's just a plain string...?
+st :: String
+st = [s|
+        Hello World #{someone}
+    |]
+
+main :: IO ()
+main = print $ st
