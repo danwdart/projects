aabed97ff236d66d91586b9c260ffd07ea7f3318
Dan Dart <git@dandart.co.uk>
2023-06-07 23:36:08 +0100

    base skeletons for callstack and classes1

---
+++ haskell/other/onlybase/src/callstack.hs
@@ -0,0 +1,17 @@
+{-# OPTIONS_GHC -Wno-redundant-constraints #-}
+
+module Main where
+
+import GHC.Stack
+
+die :: a
+die = error "Died"
+
+passDie :: HasCallStack => IO a
+passDie = die
+
+num :: HasCallStack => IO Int
+num = passDie
+
+main :: IO ()
+main = print =<< num
