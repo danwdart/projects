1df3140977633c37c1f1ff59b30a336e10ced0a0
Dan Dart <git@dandart.co.uk>
2023-07-27 11:26:07 +0100

    fix the wasm direct

--- haskell/other/wasm-direct/shell.nix
+++ haskell/other/wasm-direct/shell.nix
@@ -1,4 +1,5 @@
-with import "https://gitlab.haskell.org/ghc/ghc-wasm-meta/-/archive/master/ghc-wasm-meta-master.tar.gz" {};
+# import "https://gitlab.haskell.org/ghc/ghc-wasm-meta/-/archive/master/ghc-wasm-meta-master.tar.gz"
+with import <nixpkgs> {};
 runCommand "haskell" {
     buildInputs = [
         #clang
@@ -6,7 +7,8 @@ runCommand "haskell" {
         #emscripten
         #binaryen
         gcc # for strip
-        (haskell.packages.ghcHEAD.ghcWithPackages (self: with self; [
+        (haskell.packages.ghc962.ghcWithPackages (self: with self; [
+            cabal-install
         ]))
     ];
 } ""
