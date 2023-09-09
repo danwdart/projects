e5075209eaa46bdb61bcc51c6476e5e85f48a1a4
Dan Dart <git@dandart.co.uk>
2023-09-09 01:08:59 +0100

    maths ints

---
+++ haskell/maths/src/ints.hs
@@ -0,0 +1,34 @@
+module Main where
+
+-- import Data.List qualified as L
+import Data.Map qualified as M
+import Data.Set qualified as S
+import Data.Word
+
+weirdSequence :: [Word8]
+weirdSequence = take 20 $ iterate (+ 100) 100
+
+maps :: Word8 -> [(Word8, Word8)]
+maps quotient = [(x, y) | x <- [1..maxBound] :: [Word8], y <- [1..maxBound], x * y * quotient == x] -- meaning y = 1/q
+
+specificMaps :: Word8 -> [(Word8, Word8)]
+specificMaps quotient = [(quotient * x, y) | x <- [1..(maxBound `div` quotient)] :: [Word8], y <- [1..maxBound], quotient * x * y == x] -- meaning y = 1/q
+
+
+-- could probably accum map but i'm lazy
+
+accum :: (Ord a, Ord b) => [(a, b)] -> M.Map a (S.Set b)
+accum [] = M.empty
+accum ((k, v):xs) = M.insertWith S.union k (S.singleton v) (accum xs)
+
+summarise :: (Show k, Show v) =>  M.Map k (S.Set v) -> M.Map k String
+summarise = M.mapWithKey (\k v -> show k <> ": " <> show (S.size v) <> " (" <> show v <> ")")
+
+speakOfIt :: IO ()
+speakOfIt = mapM_ putStrLn . summarise . accum $ maps 5
+
+speakOfThat :: IO ()
+speakOfThat = mapM_ putStrLn . summarise . accum $ specificMaps 5
+
+main :: IO ()
+main = pure ()
