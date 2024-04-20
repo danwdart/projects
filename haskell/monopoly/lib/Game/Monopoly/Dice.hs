f7a2b9327282c81b6ed5ead9f6359590f7cfc411
Dan Dart <git@dandart.co.uk>
2024-04-21 00:19:37 +0100

    monopoly: fix build

--- haskell/monopoly/lib/Game/Monopoly/Dice.hs
+++ haskell/monopoly/lib/Game/Monopoly/Dice.hs
@@ -1,9 +1,10 @@
 module Game.Monopoly.Dice where
 
+import Control.Monad.IO.Class
 import Control.Monad.Random
 
-randomRoll ∷ MonadRandom m ⇒ m (Int, Bool)
+randomRoll ∷ (MonadRandom m, MonadIO m) ⇒ m (Int, Bool)
 randomRoll = do
-    die1 <- randomR (1, 6)
-    die2 <- randomR (1, 6)
+    die1 <- randomRIO (1, 6) -- randomR wouldn't cut it
+    die2 <- randomRIO (1, 6)
     pure (die1 + die2, die1 == die2)
--- haskell/monopoly/src/Main.hs
+++ haskell/monopoly/src/Main.hs
@@ -4,16 +4,16 @@ import Control.Monad
 import Data.Set                  qualified as Set
 import Game.Monopoly.Board
 import Game.Monopoly.Board.Devon
-import Game.Monopoly.Colour
+-- import Game.Monopoly.Colour
 import Game.Monopoly.Dice
 import Game.Monopoly.Game
 import Game.Monopoly.Helpers
 import Game.Monopoly.Player
-import Game.Monopoly.Random
+-- import Game.Monopoly.Random
 import Game.Monopoly.Rules
-import Game.Monopoly.Space
+-- import Game.Monopoly.Space
 import Game.Monopoly.Token
-import System.Random
+-- import System.Random
 
 myGame ∷ Game
 myGame = Game
@@ -32,18 +32,18 @@ performRound (Game board' players' rules') = do
     putStrLn "Let's go!"
     -- let numSpaces = length (spaces board)
     newPlayers' <- forM players' $ \player' -> do
-        putStrLn $ name player' <> (" (the " <> (show (token player') <> (") is on " <> show (playerSpace (spaces board') player'))))
-        (roll, isDouble) <- randomRoll
+        putStrLn $ name player' <> (" (the " <> (show (token player') <> (") is on " <> show (playerSpace (_spaces board') player'))))
+        (roll, _isDouble) <- randomRoll
         putStrLn $ "Roll: " <> show roll
         let newPosition = position player' + roll
         --when (newPosition > numSpaces) $ do
         --   print "Passed GO."
-        let newPlayer' = p { position = newPosition }
-        let newSpace = spaces board' !! newPosition
+        let newPlayer' = player' { position = newPosition }
+        let newSpace = _spaces board' !! newPosition
         putStrLn $ "That puts you on position " <> (show newPosition <> (" which is " <> show newSpace))
         (_, _) <- processLand newPlayer' board' newSpace
         -- board'
-        pure player'
+        pure newPlayer'
 
     pure $ Game board' newPlayers' rules'
 
