725cc7d2931e4b318eb2f54e83eaf37b773976b4
Dan Dart <git@dandart.co.uk>
2024-07-18 17:19:26 +0100

    nonary: newtype

--- haskell/maths/src/nonary.hs
+++ haskell/maths/src/nonary.hs
@@ -8,7 +8,7 @@ import GHC.TypeNats
 -- data Nonary = Zero | One | Two | Three | Four | Five | Six | Seven | Eight
 
 type InBase :: Nat → Type → Type
-data InBase n a = InBase a
+newtype InBase n a = InBase a
 
 instance Num (InBase n a) where
     InBase x + InBase y = mod (natVal _) $ x + y
