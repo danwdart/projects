9ac0f876b074acdd0c17f81b834ab5126bcd14b9
Dan Dart <git@dandart.co.uk>
2024-09-03 02:20:39 +0100

    maths: fix tests and add oeisA177715

---
+++ haskell/maths/src/oeis/A177715.hs
@@ -0,0 +1,12 @@
+{-# OPTIONS_GHC -Wno-x-partial #-}
+
+import Data.Number.CReal
+import Data.Text qualified as T
+import Data.Text.Internal.Search
+
+wellwherearethey :: [Int]
+wellwherearethey =  [(\xs -> if null xs then (-1) else head xs) . indices (T.pack . show @Integer . round $ x) . T.pack . filter (/= '.') . showCReal 1000 $ sqrt x | x <- [1..100] ]
+
+main :: IO ()
+main = print wellwherearethey
+
