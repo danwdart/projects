639543d4cdf154245c9a5109e45ce8f9c08bfcb6
Dan Dart <git@dandart.co.uk>
2024-09-28 01:59:37 +0100

    whatcoffee: upgrade to ghc 9.10

--- haskell/whatcoffee/default.nix
+++ haskell/whatcoffee/default.nix
@@ -4,7 +4,7 @@
     nixpkgs = nixpkgs;
     compiler = compiler;
   },
-  compiler ? "ghc98"
+  compiler ? "ghc910"
 }:
 let
   gitignore = nixpkgs.nix-gitignore.gitignoreSourcePure [ ./.gitignore ];
