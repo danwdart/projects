bfb363adcc6bcd056354f1a2f63d96e034ab43ce
Dan Dart <git@dandart.co.uk>
2024-11-02 15:25:43 +0000

    the type in base thing

--- haskell/maths/src/nonary.hs
+++ haskell/maths/src/nonary.hs
@@ -1,21 +1,26 @@
-
 module Main (main) where
 
 import Data.Kind
+import Data.Proxy
 import GHC.TypeNats
 
 -- data Nonary = Zero | One | Two | Three | Four | Five | Six | Seven | Eight
 
 type InBase :: Nat → Type → Type
 newtype InBase n a = InBase a
+    deriving stock (Show)
 
-instance Num (InBase n a) where
-    InBase x + InBase y = mod (natVal _) $ x + y
-    InBase x * InBase y = mod (natVal _) $ x + y
-    abs (InBase x) = _
-    signum (InBase x) = _
-    fromInteger x = InBase (mod _ x)
-    negate x = InBase _
+instance (KnownNat n, Integral a) => Num (InBase n a) where
+    InBase a1 + InBase a2 = InBase (mod (a1 + a2) (fromIntegral (natVal (Proxy :: Proxy n))))
+    InBase a1 * InBase a2 = InBase (mod (a1 * a2) (fromIntegral (natVal (Proxy :: Proxy n))))
+    abs a = a
+    signum _ = InBase 1
+    fromInteger a = InBase (fromIntegral $ mod a (fromIntegral (natVal (Proxy :: Proxy n))))
+    negate (InBase a) = InBase ((fromIntegral (natVal (Proxy :: Proxy n))) - a)
+
+-- >>> :t InBase 2 + InBase 29 :: InBase 9 Int
 
 main ∷ IO ()
-main = pure ()
+main = do
+    print $ (2 + 8 :: InBase 3 Int)
+    print $ (22 * 92 :: InBase 100 Int)
