7ec2bf0cc1e0e06cafb2e0b2b6ca7ca85850d0bd
Dan Dart <git@dandart.co.uk>
2024-11-19 20:11:44 +0000

    lazy io idk

--- haskell/other/onlybase/src/lazy-io.hs
+++ haskell/other/onlybase/src/lazy-io.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE Unsafe #-}
-{-# OPTIONS_GHC -Wno-unsafe #-}
+{-# OPTIONS_GHC -Wno-unsafe -Wno-unused-top-binds #-}
 
 module Main (main) where
 
@@ -10,21 +10,43 @@ data A = A {
     b :: String
 }
 
+data A2 = A2 {
+    a2 :: Int,
+    b2 :: String
+}
+
 data B = B {
     c :: Int,
     d :: A
 }
 
+data B2 = B2 {
+    c2 :: Int,
+    d2 :: A2
+}
+
+mkA2 ∷ A2
+mkA2 = trace "Making A2" $ A2 1 "Yo"
+
+mkB2 ∷ A2 → B2
+mkB2 = trace "Making B2" . B2 1
+
 mkAIO ∷ IO A
-mkAIO = pure . trace "Making A" $ A 1 "Yo"
+mkAIO = trace "mkAIO" . pure . trace "Making A" $ A 1 "Yo"
 
 mkBIO ∷ IO B
 mkBIO = do
-    trace "Making B" . B 1 <$> mkAIO
+    trace "Making B in IO" . B 1 <$> mkAIO
 
 main ∷ IO ()
 main = do
-    b <- mkBIO
-    print $ c b
-    b2 <- mkBIO
-    print $ c b2
+    let b2_1 = mkB2 mkA2
+    print $ c2 b2_1
+    bIO <- mkBIO
+    print $ c bIO
+
+    putStrLn "Again!!"
+    let b2_2 = mkB2 mkA2
+    print $ c2 b2_2
+    bIO2 <- mkBIO
+    print $ c bIO2
