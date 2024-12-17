b5ad450ed2a3e72d2223fad4d4a5574bc0fdc2d2
Dan Dart <git@dandart.co.uk>
2024-12-17 15:43:50 +0000

    anglo things maybe

--- haskell/anglo/lib/Word.hs
+++ haskell/anglo/lib/Word.hs
@@ -4,6 +4,7 @@
 
 module Word where
 
+import Control.Monad (unless)
 import Control.Monad.Random
 import Control.Monad.Reader
 import Control.Monad.State
@@ -11,16 +12,22 @@ import Control.Monad.Writer
 import Data.Aeson
 import GHC.Generics
 
-data Letter = Vowel | Consonant deriving (Eq, Show, Enum, Bounded)
+data Letter = Vowel | Consonant deriving stock (Eq, Show, Enum, Bounded)
 
 data Letters = Letters {
     vowels     :: [String],
     consonants :: [String]
 }
 
+getLetters ∷ Letter → Letters → [String]
+getLetters Vowel     = vowels
+getLetters Consonant = consonants
+
 instance FromJSON Letters where
     parseJSON (Array [v, c]) = Letters <$> parseJSON v <*> parseJSON c
-    parseJSON x = fail $ "Can't decode: " <> show x
+    parseJSON x              = fail $ "Can't decode: " <> show x
+
+data Location = Start | Middle | End
 
 data Phonemes = Phonemes {
     start  :: Letters,
@@ -29,6 +36,13 @@ data Phonemes = Phonemes {
 }
     deriving stock (Generic)
     deriving anyclass (FromJSON)
+
+-- should we be using smart maps here
+-- or just mapping by function??? because functions are values
+getByLoc ∷ Location → Phonemes → Letters
+getByLoc Start  = start
+getByLoc Middle = middle
+getByLoc End    = end
 
 switch ∷ Letter → Letter
 switch Vowel     = Consonant
@@ -39,13 +53,25 @@ switch Consonant = Vowel
 
 mkWord ∷ (MonadRandom m, MonadReader Phonemes m) ⇒ m String
 mkWord = do
-    letter <- _
-    concat <$> execWriter $ evalState letter genWord
+    letter' <- ask
+    pure <$> execWriter $ evalState letter' genWord
 
-genWord ∷ (MonadReader Phonemes m, MonadState Letter m, MonadWriter [String] m) ⇒ Int → m ()
-genWord length = do
-    letter <- get
-    tell ["a"]
+
+location ∷ Int → Int → Location
+location maxLength loc
+    | loc == 0 = Start
+    | loc == maxLength - 1 = End
+    | otherwise = Middle
+
+genWord ∷ (MonadRandom m, MonadReader Phonemes m, MonadState Letter m, MonadWriter [String] m) ⇒ Int → Int → m ()
+genWord length loc = do
+    letter' <- get
+    letters <- asks $ getLetters letter' . getByLoc (location length loc)
+    nextLetter <- uniform letters
+    tell [nextLetter]
+    unless (loc == length - 1) $ do
+        modify switch
+        genWord length (loc + 1)
 
 {-
 var pick = function (arr) {
