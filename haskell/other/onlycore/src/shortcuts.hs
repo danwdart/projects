ec194d1f7d076b0f963bb81a8b538cfe8644bdd4
Dan Dart <git@dandart.co.uk>
2025-01-08 00:01:24 +0000

    onlycore: shortcutting

---
+++ haskell/other/onlycore/src/shortcuts.hs
@@ -0,0 +1,142 @@
+module Main where
+
+import Control.Monad.Cont
+import Control.Monad.Except
+import Control.Monad.IO.Class
+import Control.Monad.Trans.Maybe
+
+shortcutMaybe :: Maybe String
+shortcutMaybe = do
+    a <- Just "Hello, "
+    c <- Just "World!"
+    d <- Nothing
+    pure $ a <> c <> d
+
+shortcutMaybeFail :: MonadFail m => m String
+shortcutMaybeFail = do
+    a <- pure "Hello, "
+    c <- pure "World!"
+    d <- fail "Nah"
+    pure $ a <> c <> d
+
+shortcutMaybeT :: MaybeT IO String
+shortcutMaybeT = do
+    a <- hoistMaybe $ Just "Hello, "
+    let b = Just "World!"
+    c <- hoistMaybe b
+    d <- hoistMaybe Nothing
+    liftIO . putStrLn $ "You shouldn't see this."
+    pure $ a <> c <> d
+
+shortcutMaybeTFail :: (MonadFail m, MonadIO m) => m String
+shortcutMaybeTFail = do
+    a <- pure "Hello, "
+    c <- pure "World!"
+    d <- fail "Nope."
+    liftIO . putStrLn $ "You shouldn't see this."
+    pure $ a <> c <> d
+
+shortcutEither :: Either String Int
+shortcutEither = do
+    a <- Right 12
+    c <- Right 24
+    d <- Left "Nah"
+    pure $ a + c + d
+
+shortcutEitherError :: (MonadError String m) => m Int
+shortcutEitherError = do
+    a <- pure 12
+    c <- pure 24
+    d <- throwError "Nah"
+    pure $ a + c + d
+
+shortcutExcept :: Except String Int
+shortcutExcept = do
+    a <- liftEither $ Right 12
+    c <- liftEither $ Right 24
+    d <- liftEither <$> Left $ "Nah"
+    pure $ a + c + d
+
+shortcutExceptError :: (MonadError String m) => m Int
+shortcutExceptError = do
+    a <- pure 12
+    c <- pure 24
+    d <- throwError "Nah"
+    pure $ a + c + d
+
+shortcutExceptT :: ExceptT String IO Int
+shortcutExceptT = do
+    a <- liftEither $ Right 12
+    c <- liftEither $ pure 24
+    d <- liftEither <$> Left $ "Nah"
+    liftIO . putStrLn $ "You shouldn't see this."
+    pure $ a + c + d
+
+shortcutExceptTError :: (MonadError String m, MonadIO m) => m Int
+shortcutExceptTError = do
+    a <- liftEither $ Right 12
+    c <- liftEither $ pure 24
+    d <- liftEither <$> Left $ "Nah"
+    liftIO . putStrLn $ "You shouldn't see this."
+    pure $ a + c + d
+
+shortcutCont :: Cont String String
+shortcutCont = callCC $ \k -> do
+    a <- pure 'a'
+    c <- pure 'c'
+    d <- k "How dare you!"
+    pure [a, c, d]
+
+shortcutContMC :: MonadCont m => m String
+shortcutContMC = callCC $ \k -> do
+    a <- pure 'a'
+    c <- pure 'c'
+    d <- k "How dare you!"
+    pure [a, c, d]
+
+shortcutContT :: ContT String IO String
+shortcutContT = callCC $ \k -> do
+    a <- pure 'a'
+    c <- pure 'c'
+    d <- k "How dare you!"
+    liftIO . putStrLn $ "You shouldn't see this."
+    pure [a, c, d]
+
+shortcutContTMC :: (MonadCont m, MonadIO m) => m String
+shortcutContTMC = callCC $ \k -> do
+    a <- pure 'a'
+    c <- pure 'c'
+    d <- k "How dare you!"
+    liftIO . putStrLn $ "You shouldn't see this."
+    pure [a, c, d]
+
+main :: IO ()
+main = do
+    putStrLn "Maybe"
+    print shortcutMaybe
+    putStrLn "Maybe by MonadFail"
+    print (shortcutMaybeFail :: Maybe String) -- or Fail passes through
+    putStrLn "MaybeT"
+    print =<< runMaybeT shortcutMaybeT
+    putStrLn "MaybeT by MonadFail"
+    print =<< runMaybeT shortcutMaybeTFail
+    putStrLn "Either"
+    print shortcutEither
+    putStrLn "Either by MonadError"
+    print (shortcutEitherError :: Either String Int)
+    putStrLn "Except"
+    print $ runExcept shortcutExcept
+    putStrLn "Except by MonadError"
+    print $ runExcept (shortcutExceptError :: Except String Int)
+    putStrLn "ExceptT"
+    print =<< runExceptT shortcutExceptT
+    putStrLn "ExceptT by MonadError"
+    print =<< runExceptT shortcutExceptTError
+    putStrLn "Cont"
+    print $ evalCont shortcutCont
+    putStrLn "Cont by MonadCont"
+    print $ evalCont shortcutContMC
+    putStrLn "ContT"
+    print =<< evalContT shortcutContT
+    putStrLn "ContT by MonadCont"
+    print =<< evalContT shortcutContTMC
