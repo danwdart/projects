81d9b1c54ef5b2147ceede9fb4b80b94af5c402f
Dan Dart <git@dandart.co.uk>
2025-01-24 21:42:44 +0000

    cards updates

--- haskell/cards/lib/Data/Statistics.hs
+++ haskell/cards/lib/Data/Statistics.hs
@@ -6,7 +6,7 @@ import Data.Map             as M
 
 -- combinator
 countFreq ∷ (Traversable t, Num n, Ord a) ⇒ t a → M.Map a n
-countFreq = Prelude.foldl (\m v -> M.insertWith (+) v 1 m) M.empty
+countFreq = Prelude.foldl' (\m v -> M.insertWith (+) v 1 m) M.empty
 
 -- TODO compose for <$>
 
@@ -19,4 +19,4 @@ mean xs = fromIntegral (sum xs) / fromIntegral (length xs)
 -- weighted average
 
 meanDist ∷ M.Map Int Int → Double
-meanDist = uncurry (/) . Prelude.foldl (\(v1, t1) (v2, t2) -> (v1 + v2 * t2, t1 + t2)) (0, 0) . fmap (bimap fromIntegral fromIntegral) . M.toList
+meanDist = uncurry (/) . Prelude.foldl' (\(v1, t1) (v2, t2) -> (v1 + v2 * t2, t1 + t2)) (0, 0) . fmap (bimap fromIntegral fromIntegral) . M.toList
