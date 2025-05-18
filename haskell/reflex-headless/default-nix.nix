6718b3edfe8c8af0bb15302442e00e26b222bb6b
Dan Dart <git@dandart.co.uk>
2025-05-18 15:13:35 +0100

    reflex-headless js

--- haskell/reflex-headless/default-nix.nix
+++ haskell/reflex-headless/default-nix.nix
@@ -4,7 +4,7 @@
     nixpkgs = nixpkgs;
     compiler = compiler;
   },
-  compiler ? "ghc910"
+  compiler ? "ghc912"
 }:
 let
   gitignore = nixpkgs.nix-gitignore.gitignoreSourcePure [ ./.gitignore ];
