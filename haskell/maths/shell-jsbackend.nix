8354c76aa557d76d45d9f8dadae5f29206e6eaaa
Dan Dart <git@dandart.co.uk>
2025-12-20 22:46:49 +0000

    maths: update to ghc 9.14

--- haskell/maths/shell-jsbackend.nix
+++ haskell/maths/shell-jsbackend.nix
@@ -1,9 +1,9 @@
-with import <nixpkgs> {};
+with import (builtins.fetchTarball "https://github.com/guibou/nixpkgs/archive/ghc-914.zip") {};
 # needs mkShell in order to use headers/etc. from deps! how do we do that from nix-shell 
 mkShell rec {
     packages = [
-        pkgsCross.ghcjs.pkgsBuildHost.haskell.compiler.ghc912
-        pkgsCross.ghcjs.pkgsBuildHost.haskell.packages.ghc912.hsc2hs
+        pkgsCross.ghcjs.pkgsBuildHost.haskell.compiler.ghc914
+        pkgsCross.ghcjs.pkgsBuildHost.haskell.packages.ghc914.hsc2hs
         cabal-install
         # pkg-config
         # zlib.dev
