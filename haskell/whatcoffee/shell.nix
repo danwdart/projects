6b704fb4bb699ee1fda4dd38cd5b83818e365b5d
Dan Dart <git@dandart.co.uk>
2025-12-21 01:11:01 +0000

    different bins

--- haskell/whatcoffee/shell.nix
+++ haskell/whatcoffee/shell.nix
@@ -11,7 +11,7 @@ mkShell rec {
     ];
     shellHook = ''
         # no krank yet
-        [[ -f ~/.local/bin/refactor ]] || cabal install apply-refact cabal-add cabal-fmt doctest ghci-dap ghcid ghcide haskell-debug-adapter haskell-language-server hasktags hlint hoogle hpack implicit-hie stan stylish-haskell weeder --allow-newer
+        [[ -f ~/.local/bin/ghcid ]] || cabal install cabal-add cabal-fmt ghcid haskell-debug-adapter hasktags hoogle hpack implicit-hie krank --allow-newer
         export PATH=~/.local/bin:$PATH
         gen-hie > hie.yaml
         for i in $(find . -type f | grep -v "dist-*"); do krank $i; done
