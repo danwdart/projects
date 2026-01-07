2720776ffe80c50329df6b3b6ce6e9fbf22d1529
Dan Dart <git@dandart.co.uk>
2026-01-07 00:13:24 +0000

    shell-musl.nix

--- haskell/projecteuler/shell-musl.nix
+++ haskell/projecteuler/shell-musl.nix
@@ -11,7 +11,7 @@ mkShell rec {
     ];
     # shellHook = ''
     #     # no krank yet
-        [[ -f ~/.local/bin/ghcid ]] || cabal install cabal-add cabal-fmt ghcid haskell-debug-adapter hasktags hoogle hpack implicit-hie krank --allow-newer
+    #     [[ -f ~/.local/bin/ghcid ]] || cabal install cabal-add cabal-fmt ghcid haskell-debug-adapter hasktags hoogle hpack implicit-hie krank --allow-newer
     #     export PATH=~/.local/bin:$PATH
     #     gen-hie > hie.yaml
     #     for i in $(find . -type f | grep -v "dist-*"); do krank $i; done
