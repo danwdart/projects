198797c5c9c86587df7c428f9175ee38b35e4b51
Dan Dart <git@dandart.co.uk>
2026-04-17 20:13:59 +0000

    napier todo

--- haskell/maths/src/napier-locar.hs
+++ haskell/maths/src/napier-locar.hs
@@ -24,3 +24,10 @@ main ∷ IO ()
 main = traverse_ (\word ->
     putStrLn $ "The word " <> word <> " translated to Napier notation is equal to the value " <> show (symToInt word)
     ) alphabeticalWords
+
+
+-- Now do the chessboard simulation - ridiculous.
+
+-- How may one render, probably by going [[65536],[32768,32768],[16384,16384,16384]]...
+
+-- are we gonna say it's infinite idk - that means n copies of 2^n-1 or equivalently n+1 copies of 2^n?
