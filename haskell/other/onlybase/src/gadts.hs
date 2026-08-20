cd347ffc96202cbeb1b90388c1291022e141de84
Ember Dart <git@emberdart.co.uk>
2026-08-21 00:48:07 +0100

    ADT equivalents to GADTs

--- haskell/other/onlybase/src/gadts.hs
+++ haskell/other/onlybase/src/gadts.hs
@@ -4,38 +4,80 @@ module Main (main) where
 
 import Data.DisparateList
 
-data G a where
-    GBool :: G Bool
-    GInt :: G Int
+data BoolOrIntG a where
+    BoolG :: BoolOrIntG Bool
+    IntG :: BoolOrIntG Int
 
-f ∷ G a → a
-f GBool = False
-f GInt  = 0
+deriving stock instance Show (BoolOrIntG a)
 
-data Arrow a b where
-    Fn :: (a → b) -> Arrow a b
-    Comp :: Arrow b c -> Arrow a b -> Arrow a c
+data BoolOrIntA a
+    = BoolA (Bool -> a)
+    | IntA (Int -> a)
 
-eval ∷ Arrow a b → a → b
-eval (Fn f') x     = f' x
-eval (Comp f' g) x = eval f' (eval g x)
+instance Show (BoolOrIntA a) where
+    show (BoolA _) = "BoolA"
+    show (IntA _) = "IntA"
 
-num ∷ Arrow a b → Int
-num (Fn _)      = 0
-num (Comp f' g) = 1 + num f' + num g
+extBIG ∷ BoolOrIntG a → a
+extBIG BoolG = False
+extBIG IntG  = 0
 
-hmm ∷ Enum a ⇒ Arrow a a
-hmm = Comp (Comp (Fn succ) (Fn succ)) (Comp (Fn succ) (Fn succ))
+extBIA :: BoolOrIntA a -> a
+extBIA (BoolA f) = f False
+extBIA (IntA f) = f 0
 
-ids ∷ Arrow a a
-ids = Comp (Comp (Fn id) (Fn id)) (Comp (Fn id) (Fn id))
+data ArrowG a b where
+    FnG :: (a → b) -> ArrowG a b
+    CompG :: ArrowG b c -> ArrowG a b -> ArrowG a c
+
+-- ??? something like that...
+data ArrowA a b
+    = forall c. FnA (a -> b) (c -> b) (a -> c)
+    | forall c d. CompA (ArrowA b c) (ArrowA a b) (d -> c) (a -> d) -- where rbc and rab are something to be determined
+
+evalG ∷ ArrowG a b → a → b
+evalG (FnG f') x     = f' x
+evalG (CompG f' g) x = evalG f' (evalG g x)
+
+evalA :: ArrowA a b -> a -> b
+evalA (FnA f' _ _) x = f' x
+evalA (CompA _ _ _ _) _ = undefined
+
+numG ∷ ArrowG a b → Int
+numG (FnG _)      = 0
+numG (CompG f' g) = 1 + numG f' + numG g
+
+numA :: ArrowA a b -> Int
+numA (FnA _ _ _) = 0
+numA (CompA f' g' _ _) = 1 + numA f' + numA g'
+
+hmmG ∷ Enum a ⇒ ArrowG a a
+hmmG = CompG (CompG (FnG succ) (FnG succ)) (CompG (FnG succ) (FnG succ))
+
+hmmA :: ArrowA a a
+hmmA = undefined -- CompA (CompA (FnA succ) (FnA succ)) (CompA (FnA succ) (FnA succ))
+
+idsG ∷ ArrowG a a
+idsG = CompG (CompG (FnG id) (FnG id)) (CompG (FnG id) (FnG id))
+-- deriving instance (Show a, Show (L t)) => Show (L (a ': t))
+
+idsA ∷ ArrowA a a
+idsA = undefined -- CompA (CompA (FnA id) (FnA id)) (CompA (FnA id) (FnA id))
 -- deriving instance (Show a, Show (L t)) => Show (L (a ': t))
 
 main ∷ IO ()
 main = do
-    print $ f GBool
-    print $ f GInt
+    print $ extBIG BoolG
+    print $ extBIG IntG
+
     print ((2::Int) :> "a" :> Nil)
     print Nil
-    print $ eval hmm (1::Int)
-    print $ num ids
+    
+    print $ evalG hmmG (1::Int)
+    print $ numG idsG
+
+    print $ extBIA $ BoolA id
+    print $ extBIA $ IntA id
+    
+    -- print $ evalA hmmA (1::Int)
+    -- print $ numA idsA
